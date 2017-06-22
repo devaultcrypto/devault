7dd4cc43e30fa4f9352fcb04b50396bd3f4ab883
Wladimir J. van der Laan <laanwj@gmail.com>
2017-06-22 17:10:16 +0200

    Merge #10530: Fix invalid instantiation and possibly unsafe accesses of array in class base_uint<BITS>
    Summary:
    e5c6168 Fix instantiation and array accesses in class base_uint<BITS> (Pavlos Antoniou)
    
    Tree-SHA512: e4d39510d776c5ae8814cd5fb5c5d183cd8da937e339bff95caff68a84492fbec68bf513c5a6267446a564d39093e0c7fc703c645b511caab80f7baf7955b804
    
    Backport of Core PR10530
    https://github.com/bitcoin/bitcoin/pull/10530
    
    Test Plan:
      make check
      test_runner.py
    
    Reviewers: deadalnix, Fabien, jasonbcox, markblundeberg, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: Fabien, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D3569

--- src/arith_uint256.h
+++ src/arith_uint256.h
@@ -9,6 +9,7 @@
 #include <cassert>
 #include <cstdint>
 #include <cstring>
+#include <limits>
 #include <stdexcept>
 #include <string>
 
@@ -27,11 +28,19 @@ protected:
 
 public:
     base_uint() {
+        static_assert(
+            BITS / 32 > 0 && BITS % 32 == 0,
+            "Template parameter BITS must be a positive multiple of 32.");
+
         for (int i = 0; i < WIDTH; i++)
             pn[i] = 0;
     }
 
     base_uint(const base_uint &b) {
+        static_assert(
+            BITS / 32 > 0 && BITS % 32 == 0,
+            "Template parameter BITS must be a positive multiple of 32.");
+
         for (int i = 0; i < WIDTH; i++)
             pn[i] = b.pn[i];
     }
@@ -43,6 +52,10 @@ public:
     }
 
     base_uint(uint64_t b) {
+        static_assert(
+            BITS / 32 > 0 && BITS % 32 == 0,
+            "Template parameter BITS must be a positive multiple of 32.");
+
         pn[0] = (unsigned int)b;
         pn[1] = (unsigned int)(b >> 32);
         for (int i = 2; i < WIDTH; i++)
@@ -145,7 +158,7 @@ public:
     base_uint &operator++() {
         // prefix operator
         int i = 0;
-        while (++pn[i] == 0 && i < WIDTH - 1)
+        while (i < WIDTH && ++pn[i] == 0)
             i++;
         return *this;
     }
@@ -160,7 +173,7 @@ public:
     base_uint &operator--() {
         // prefix operator
         int i = 0;
-        while (--pn[i] == (uint32_t)-1 && i < WIDTH - 1)
+        while (i < WIDTH && --pn[i] == std::numeric_limits<uint32_t>::max())
             i++;
         return *this;
     }
