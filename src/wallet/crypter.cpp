405a7067c6e87c4324e8b538f97180a05e1ed2a1
Jonas Schnelli <dev@jonasschnelli.ch>
2017-10-06 20:56:43 -0700

    Fix code style in keystore.cpp/crypter.cpp
    Summary: This is from Core PR11272
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, markblundeberg
    
    Reviewed By: #bitcoin_abc, markblundeberg
    
    Subscribers: markblundeberg
    
    Differential Revision: https://reviews.bitcoinabc.org/D3119

--- src/wallet/crypter.cpp
+++ src/wallet/crypter.cpp
@@ -184,12 +184,8 @@ bool CCryptoKeyStore::IsLocked() const {
     if (!IsCrypted()) {
         return false;
     }
-    bool result;
-    {
-        LOCK(cs_KeyStore);
-        result = vMasterKey.empty();
-    }
-    return result;
+    LOCK(cs_KeyStore);
+    return vMasterKey.empty();
 }
 
 bool CCryptoKeyStore::Lock() {
@@ -258,41 +254,36 @@ bool CCryptoKeyStore::Unlock(const CKeyingMaterial &vMasterKeyIn) {
 }
 
 bool CCryptoKeyStore::AddKeyPubKey(const CKey &key, const CPubKey &pubkey) {
-    {
-        LOCK(cs_KeyStore);
-        if (!IsCrypted()) {
-            return CBasicKeyStore::AddKeyPubKey(key, pubkey);
-        }
+    LOCK(cs_KeyStore);
+    if (!IsCrypted()) {
+        return CBasicKeyStore::AddKeyPubKey(key, pubkey);
+    }
 
-        if (IsLocked()) {
-            return false;
-        }
+    if (IsLocked()) {
+        return false;
+    }
 
-        std::vector<uint8_t> vchCryptedSecret;
-        CKeyingMaterial vchSecret(key.begin(), key.end());
-        if (!EncryptSecret(vMasterKey, vchSecret, pubkey.GetHash(),
-                           vchCryptedSecret)) {
-            return false;
-        }
+    std::vector<uint8_t> vchCryptedSecret;
+    CKeyingMaterial vchSecret(key.begin(), key.end());
+    if (!EncryptSecret(vMasterKey, vchSecret, pubkey.GetHash(),
+                       vchCryptedSecret)) {
+        return false;
+    }
 
-        if (!AddCryptedKey(pubkey, vchCryptedSecret)) {
-            return false;
-        }
+    if (!AddCryptedKey(pubkey, vchCryptedSecret)) {
+        return false;
     }
     return true;
 }
 
 bool CCryptoKeyStore::AddCryptedKey(
     const CPubKey &vchPubKey, const std::vector<uint8_t> &vchCryptedSecret) {
-    {
-        LOCK(cs_KeyStore);
-        if (!SetCrypted()) {
-            return false;
-        }
-
-        mapCryptedKeys[vchPubKey.GetID()] =
-            make_pair(vchPubKey, vchCryptedSecret);
+    LOCK(cs_KeyStore);
+    if (!SetCrypted()) {
+        return false;
     }
+
+    mapCryptedKeys[vchPubKey.GetID()] = make_pair(vchPubKey, vchCryptedSecret);
     return true;
 }
 
@@ -305,18 +296,16 @@ bool CCryptoKeyStore::HaveKey(const CKeyID &address) const {
 }
 
 bool CCryptoKeyStore::GetKey(const CKeyID &address, CKey &keyOut) const {
-    {
-        LOCK(cs_KeyStore);
-        if (!IsCrypted()) {
-            return CBasicKeyStore::GetKey(address, keyOut);
-        }
+    LOCK(cs_KeyStore);
+    if (!IsCrypted()) {
+        return CBasicKeyStore::GetKey(address, keyOut);
+    }
 
-        auto mi = mapCryptedKeys.find(address);
-        if (mi != mapCryptedKeys.end()) {
-            const CPubKey &vchPubKey = (*mi).second.first;
-            const std::vector<uint8_t> &vchCryptedSecret = (*mi).second.second;
-            return DecryptKey(vMasterKey, vchCryptedSecret, vchPubKey, keyOut);
-        }
+    auto mi = mapCryptedKeys.find(address);
+    if (mi != mapCryptedKeys.end()) {
+        const CPubKey &vchPubKey = (*mi).second.first;
+        const std::vector<uint8_t> &vchCryptedSecret = (*mi).second.second;
+        return DecryptKey(vMasterKey, vchCryptedSecret, vchPubKey, keyOut);
     }
     return false;
 }
@@ -337,7 +326,6 @@ bool CCryptoKeyStore::GetPubKey(const CKeyID &address,
         // Check for watch-only pubkeys
         return CBasicKeyStore::GetPubKey(address, vchPubKeyOut);
     }
-    return false;
 }
 
 std::set<CKeyID> CCryptoKeyStore::GetKeys() const {
@@ -357,28 +345,26 @@ std::set<CKeyID> CCryptoKeyStore::GetKeys() const {
 // MapKeys are for imported private keys
 // Now just use SetCrypted instead - keep this for a bit
 bool CCryptoKeyStore::EncryptKeys(CKeyingMaterial &vMasterKeyIn) {
-    {
-        LOCK(cs_KeyStore);
-        if (!mapCryptedKeys.empty() || IsCrypted()) {
+    LOCK(cs_KeyStore);
+    if (!mapCryptedKeys.empty() || IsCrypted()) {
+        return false;
+    }
+
+    fUseCrypto = true;
+    for (const KeyMap::value_type &mKey : mapKeys) {
+        const CKey &key = mKey.second;
+        CPubKey vchPubKey = key.GetPubKey();
+        CKeyingMaterial vchSecret(key.begin(), key.end());
+        std::vector<uint8_t> vchCryptedSecret;
+        if (!EncryptSecret(vMasterKeyIn, vchSecret, vchPubKey.GetHash(),
+                           vchCryptedSecret)) {
             return false;
         }
-
-        fUseCrypto = true;
-        for (KeyMap::value_type &mKey : mapKeys) {
-            const CKey &key = mKey.second;
-            CPubKey vchPubKey = key.GetPubKey();
-            CKeyingMaterial vchSecret(key.begin(), key.end());
-            std::vector<uint8_t> vchCryptedSecret;
-            if (!EncryptSecret(vMasterKeyIn, vchSecret, vchPubKey.GetHash(),
-                               vchCryptedSecret)) {
-                return false;
-            }
-            if (!AddCryptedKey(vchPubKey, vchCryptedSecret)) {
-                return false;
-            }
+        if (!AddCryptedKey(vchPubKey, vchCryptedSecret)) {
+            return false;
         }
-        mapKeys.clear();
     }
+    mapKeys.clear();
     return true;
 }
 
