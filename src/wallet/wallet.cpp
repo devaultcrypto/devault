823bb3e210d4703fc27bc3fefb2e19a9357b1e5f
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-09 14:22:22 +0100

    Merge #10368: [wallet] Remove helper conversion operator from wallet
    Summary:
    5a5e4e9 [wallet] Remove CTransaction&() helper conversion operator from wallet implementation. (Karl-Johan Alm)
    
    Pull request description:
    
      The `CTransaction&()` operator in `CMerkleTx` makes conversion into `CTransaction`s transparent, but was marked as to-be-removed in favor of explicitly getting the `tx` ivar, presumably as the operator can lead to ambiguous behavior and makes the code harder to follow.
    
      This PR removes the operator and adapts callers. This includes some cases of `static_cast<CTransaction>(wtx)` → `*wtx.tx`, which is definitely an improvement.
    
    Tree-SHA512: 95856fec7194d6a79615ea1c322abfcd6bcedf6ffd0cfa89bbdd332ce13035fa52dd4b828d20df673072dde1be64b79c513529a6f422dd5f0961ce722a32d56a
    
    Backport of Core PR 10368
    https://github.com/bitcoin/bitcoin/pull/10368/files
    Completes T588
    
    Note: The change in walletmodeltransaction.cpp is missing due to this out-of-order backport: D2618
    
    Test Plan:
    ```
    make check
    test_runner.py
    ```
    
    Reviewers: deadalnix, Fabien, #bitcoin_abc
    
    Reviewed By: deadalnix, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D2794

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1877,7 +1877,7 @@ Amount CWalletTx::GetDebit(const isminefilter &filter) const {
         if (fDebitCached) {
             debit += nDebitCached;
         } else {
-            nDebitCached = pwallet->GetDebit(*this, ISMINE_SPENDABLE);
+            nDebitCached = pwallet->GetDebit(*tx, ISMINE_SPENDABLE);
             fDebitCached = true;
             debit += nDebitCached;
         }
@@ -1887,7 +1887,7 @@ Amount CWalletTx::GetDebit(const isminefilter &filter) const {
         if (fWatchDebitCached) {
             debit += nWatchDebitCached;
         } else {
-            nWatchDebitCached = pwallet->GetDebit(*this, ISMINE_WATCH_ONLY);
+            nWatchDebitCached = pwallet->GetDebit(*tx, ISMINE_WATCH_ONLY);
             fWatchDebitCached = true;
             debit += Amount(nWatchDebitCached);
         }
@@ -1909,7 +1909,7 @@ Amount CWalletTx::GetCredit(const isminefilter &filter) const {
         if (fCreditCached) {
             credit += nCreditCached;
         } else {
-            nCreditCached = pwallet->GetCredit(*this, ISMINE_SPENDABLE);
+            nCreditCached = pwallet->GetCredit(*tx, ISMINE_SPENDABLE);
             fCreditCached = true;
             credit += nCreditCached;
         }
@@ -1919,7 +1919,7 @@ Amount CWalletTx::GetCredit(const isminefilter &filter) const {
         if (fWatchCreditCached) {
             credit += nWatchCreditCached;
         } else {
-            nWatchCreditCached = pwallet->GetCredit(*this, ISMINE_WATCH_ONLY);
+            nWatchCreditCached = pwallet->GetCredit(*tx, ISMINE_WATCH_ONLY);
             fWatchCreditCached = true;
             credit += nWatchCreditCached;
         }
@@ -1934,7 +1934,7 @@ Amount CWalletTx::GetImmatureCredit(bool fUseCache) const {
             return nImmatureCreditCached;
         }
 
-        nImmatureCreditCached = pwallet->GetCredit(*this, ISMINE_SPENDABLE);
+        nImmatureCreditCached = pwallet->GetCredit(*tx, ISMINE_SPENDABLE);
         fImmatureCreditCached = true;
         return nImmatureCreditCached;
     }
@@ -1980,8 +1980,7 @@ Amount CWalletTx::GetImmatureWatchOnlyCredit(const bool fUseCache) const {
             return nImmatureWatchCreditCached;
         }
 
-        nImmatureWatchCreditCached =
-            pwallet->GetCredit(*this, ISMINE_WATCH_ONLY);
+        nImmatureWatchCreditCached = pwallet->GetCredit(*tx, ISMINE_WATCH_ONLY);
         fImmatureWatchCreditCached = true;
         return nImmatureWatchCreditCached;
     }
@@ -2026,7 +2025,7 @@ Amount CWalletTx::GetChange() const {
         return nChangeCached;
     }
 
-    nChangeCached = pwallet->GetChange(*this);
+    nChangeCached = pwallet->GetChange(*tx);
     fChangeCached = true;
     return nChangeCached;
 }
@@ -2037,7 +2036,7 @@ bool CWalletTx::InMempool() const {
 
 bool CWalletTx::IsTrusted() const {
     // Quick answer in most cases
-    if (!CheckFinalTx(*this)) {
+    if (!CheckFinalTx(*tx)) {
         return false;
     }
 
@@ -2315,7 +2314,7 @@ void CWallet::AvailableCoins(std::vector<COutput> &vCoins, bool fOnlySafe,
         const TxId &wtxid = it.first;
         const CWalletTx *pcoin = &(it.second);
 
-        if (!CheckFinalTx(*pcoin)) {
+        if (!CheckFinalTx(*pcoin->tx)) {
             continue;
         }
 
