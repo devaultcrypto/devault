3bd810fc523a40f87ffcb8af8397dced2bfd42c1
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-18 14:11:34 +0100

    tests: move pwalletMain to wallet test fixture
    Summary:
    ```
    Scope the variable instead of using an external global; this is how test
    fixtures are intended to be used.
    
    Followup to #11713.
    ```
    
    Backport of core PR11718
    https://github.com/bitcoin/bitcoin/pull/11718/files
    
    Depends on D3097
    Completes T659
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3098

--- src/wallet/test/accounting_tests.cpp
+++ src/wallet/test/accounting_tests.cpp
@@ -8,17 +8,15 @@
 
 #include <cstdint>
 
-#include <boost/test/unit_test.hpp>
-extern std::unique_ptr<CWallet> pwalletMain;
-
 BOOST_FIXTURE_TEST_SUITE(accounting_tests, WalletTestingSetup)
 
-static void GetResults(std::map<Amount, CAccountingEntry> &results) {
+static void GetResults(CWallet *wallet,
+                       std::map<Amount, CAccountingEntry> &results) {
     std::list<CAccountingEntry> aes;
 
     results.clear();
-    BOOST_CHECK(pwalletMain->ReorderTransactions() == DBErrors::LOAD_OK);
-    pwalletMain->ListAccountCreditDebit("", aes);
+    BOOST_CHECK(wallet->ReorderTransactions() == DBErrors::LOAD_OK);
+    wallet->ListAccountCreditDebit("", aes);
     for (CAccountingEntry &ae : aes) {
         results[ae.nOrderPos * Amount::min_amount()] = ae;
     }
@@ -49,7 +47,7 @@ BOOST_AUTO_TEST_CASE(acc_orderupgrade) {
     ae.strOtherAccount = "c";
     pwalletMain->AddAccountingEntry(ae);
 
-    GetResults(results);
+    GetResults(pwalletMain.get(), results);
 
     BOOST_CHECK(pwalletMain->nOrderPosNext == 3);
     BOOST_CHECK(2 == results.size());
@@ -64,7 +62,7 @@ BOOST_AUTO_TEST_CASE(acc_orderupgrade) {
     ae.nOrderPos = pwalletMain->IncOrderPosNext();
     pwalletMain->AddAccountingEntry(ae);
 
-    GetResults(results);
+    GetResults(pwalletMain.get(), results);
 
     BOOST_CHECK(results.size() == 3);
     BOOST_CHECK(pwalletMain->nOrderPosNext == 4);
@@ -97,7 +95,7 @@ BOOST_AUTO_TEST_CASE(acc_orderupgrade) {
     vpwtx[2]->nTimeReceived = (unsigned int)1333333329;
     vpwtx[2]->nOrderPos = -1;
 
-    GetResults(results);
+    GetResults(pwalletMain.get(), results);
 
     BOOST_CHECK(results.size() == 3);
     BOOST_CHECK(pwalletMain->nOrderPosNext == 6);
@@ -114,7 +112,7 @@ BOOST_AUTO_TEST_CASE(acc_orderupgrade) {
     ae.nOrderPos = -1;
     pwalletMain->AddAccountingEntry(ae);
 
-    GetResults(results);
+    GetResults(pwalletMain.get(), results);
 
     BOOST_CHECK(results.size() == 4);
     BOOST_CHECK(pwalletMain->nOrderPosNext == 7);
