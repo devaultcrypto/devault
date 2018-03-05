7d11ee25dc3be0b8040b6d48cf8915d87cc3a838
Andrew Chow <achow101-github@achow101.com>
2018-03-05 16:37:24 -0500

    Calculate and store the number of bytes required to spend an input
    Summary: This is extracted from Core PR10637 : https://github.com/bitcoin/bitcoin/pull/10637/commits/12ec29d3bb0d46c61712210fe9bb96a0d543204a
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3243

--- src/script/sign.h
+++ src/script/sign.h
@@ -107,5 +107,6 @@ SignatureData DataFromTransaction(const CMutableTransaction &tx,
                                   unsigned int nIn);
 void UpdateTransaction(CMutableTransaction &tx, unsigned int nIn,
                        const SignatureData &data);
+void UpdateInput(CTxIn &input, const SignatureData &data);
 
 #endif // BITCOIN_SCRIPT_SIGN_H
