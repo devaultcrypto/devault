bfdda00d188ecae4d0443f3f263592e82f1f1e78
Pieter Wuille <pieter.wuille@gmail.com>
2018-03-13 16:35:01 -0700

    Merge READWRITEMANY into READWRITE
    Summary: Backported commit 1/2 (2761bca997) from PR12683
    
    Test Plan: `make check`
    
    Reviewers: #bitcoin_abc, deadalnix, Fabien, nakihito
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Maniphest Tasks: T634
    
    Differential Revision: https://reviews.bitcoinabc.org/D2930

--- src/serialize.h
+++ src/serialize.h
@@ -153,8 +153,7 @@ enum {
     SER_GETHASH = (1 << 2),
 };
 
-#define READWRITE(obj) (::SerReadWrite(s, (obj), ser_action))
-#define READWRITEMANY(...) (::SerReadWriteMany(s, ser_action, __VA_ARGS__))
+#define READWRITE(...) (::SerReadWriteMany(s, ser_action, __VA_ARGS__))
 
 /**
  * Implement three methods for serializable objects. These are actually wrappers
@@ -826,17 +825,6 @@ struct CSerActionUnserialize {
     constexpr bool ForRead() const { return true; }
 };
 
-template <typename Stream, typename T>
-inline void SerReadWrite(Stream &s, const T &obj,
-                         CSerActionSerialize ser_action) {
-    ::Serialize(s, obj);
-}
-
-template <typename Stream, typename T>
-inline void SerReadWrite(Stream &s, T &obj, CSerActionUnserialize ser_action) {
-    ::Unserialize(s, obj);
-}
-
 /**
  * ::GetSerializeSize implementations
  *
@@ -878,11 +866,6 @@ public:
 
 template <typename Stream> void SerializeMany(Stream &s) {}
 
-template <typename Stream, typename Arg>
-void SerializeMany(Stream &s, Arg &&arg) {
-    ::Serialize(s, std::forward<Arg>(arg));
-}
-
 template <typename Stream, typename Arg, typename... Args>
 void SerializeMany(Stream &s, Arg &&arg, Args &&... args) {
     ::Serialize(s, std::forward<Arg>(arg));
@@ -890,11 +873,6 @@ void SerializeMany(Stream &s, Arg &&arg, Args &&... args) {
 }
 
 template <typename Stream> inline void UnserializeMany(Stream &s) {}
-
-template <typename Stream, typename Arg>
-inline void UnserializeMany(Stream &s, Arg &arg) {
-    ::Unserialize(s, arg);
-}
 
 template <typename Stream, typename Arg, typename... Args>
 inline void UnserializeMany(Stream &s, Arg &arg, Args &... args) {
