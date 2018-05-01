c52285e478854f3c3ff0d972880053e3f8184acd
Wladimir J. van der Laan <laanwj@gmail.com>
2018-05-01 15:05:49 +0200

    net: Add option `-enablebip61` to configure sending of BIP61 notifications
    Summary:
    This commit adds a boolean option `-enablebip61`, defaulting to `1`, that
    can be used to disable the sending of BIP61 `reject` messages. This
    functionality has been requested for various reasons:
    
    - security (DoS): reject messages can reveal internal state that can be
      used to target certain resources such as the mempool more easily.
    
    - bandwidth: a typical node sends lots of reject messages; this counts
      against upstream bandwidth. Also the reject messages tend to be larger
      than the message that was rejected.
    
    On the other hand, reject messages can be useful while developing client
    software (I found them indispensable while creating bitcoin-submittx),
    as well as for our own test cases, so whatever the default becomes on the
    long run, IMO the functionality should be retained as option. But that's
    a discussion for later.
    
     * doc: Mention disabling BIP61 in bips.md
    
    This is a backport of Core PR13134
    
    Test Plan:
      make check
      ./test/functional/test_runner.py
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Subscribers: Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3722

--- test/functional/p2p_invalid_tx.py
+++ test/functional/p2p_invalid_tx.py
@@ -19,6 +19,9 @@ from test_framework.test_framework import ComparisonTestFramework
 '''
 In this test we connect to one node over p2p, and test tx requests.
 '''
+REJECT_INVALID = 16
+REJECT_PUSHONLY = b'mandatory-script-verify-flag-failed (Only push operators allowed in signature scripts)'
+
 
 # Use the ComparisonTestFramework with 1 node: only use --testbinary.
 
@@ -75,11 +78,104 @@ class InvalidTxRequestTest(ComparisonTestFramework):
 
         # b'\x64' is OP_NOTIF
         # Transaction will be rejected with code 16 (REJECT_INVALID)
+        # and we get disconnected immediately
+        self.log.info('Test a transaction that is rejected')
         tx1 = create_transaction(
-            self.block1.vtx[0], 0, b'\x64', 50 * COIN - 12000)
-        yield TestInstance([[tx1, RejectResult(16, b'mandatory-script-verify-flag-failed')]])
+            block1.vtx[0], 0, b'\x64' * 35, 50 * COIN - 12000)
+        node.p2p.send_txs_and_test([tx1], node, success=False, expect_disconnect=True,
+                                   reject_code=REJECT_INVALID, reject_reason=REJECT_PUSHONLY)
 
-        # TODO: test further transactions...
+        # Make two p2p connections to provide the node with orphans
+        # * p2ps[0] will send valid orphan txs (one with low fee)
+        # * p2ps[1] will send an invalid orphan tx (and is later disconnected for that)
+        self.reconnect_p2p(num_connections=2)
+
+        self.log.info('Test orphan transaction handling ... ')
+        # Create a root transaction that we withold until all dependend transactions
+        # are sent out and in the orphan cache
+        tx_withhold = CTransaction()
+        tx_withhold.vin.append(
+            CTxIn(outpoint=COutPoint(block1.vtx[0].sha256, 0)))
+        tx_withhold.vout.append(
+            CTxOut(nValue=50 * COIN - 12000, scriptPubKey=b'\x51'))
+        pad_tx(tx_withhold)
+        tx_withhold.calc_sha256()
+
+        # Our first orphan tx with some outputs to create further orphan txs
+        tx_orphan_1 = CTransaction()
+        tx_orphan_1.vin.append(
+            CTxIn(outpoint=COutPoint(tx_withhold.sha256, 0)))
+        tx_orphan_1.vout = [CTxOut(nValue=10 * COIN, scriptPubKey=b'\x51')] * 3
+        pad_tx(tx_orphan_1)
+        tx_orphan_1.calc_sha256()
+
+        # A valid transaction with low fee
+        tx_orphan_2_no_fee = CTransaction()
+        tx_orphan_2_no_fee.vin.append(
+            CTxIn(outpoint=COutPoint(tx_orphan_1.sha256, 0)))
+        tx_orphan_2_no_fee.vout.append(
+            CTxOut(nValue=10 * COIN, scriptPubKey=b'\x51'))
+        pad_tx(tx_orphan_2_no_fee)
+
+        # A valid transaction with sufficient fee
+        tx_orphan_2_valid = CTransaction()
+        tx_orphan_2_valid.vin.append(
+            CTxIn(outpoint=COutPoint(tx_orphan_1.sha256, 1)))
+        tx_orphan_2_valid.vout.append(
+            CTxOut(nValue=10 * COIN - 12000, scriptPubKey=b'\x51'))
+        tx_orphan_2_valid.calc_sha256()
+        pad_tx(tx_orphan_2_valid)
+
+        # An invalid transaction with negative fee
+        tx_orphan_2_invalid = CTransaction()
+        tx_orphan_2_invalid.vin.append(
+            CTxIn(outpoint=COutPoint(tx_orphan_1.sha256, 2)))
+        tx_orphan_2_invalid.vout.append(
+            CTxOut(nValue=11 * COIN, scriptPubKey=b'\x51'))
+        pad_tx(tx_orphan_2_invalid)
+
+        self.log.info('Send the orphans ... ')
+        # Send valid orphan txs from p2ps[0]
+        node.p2p.send_txs_and_test(
+            [tx_orphan_1, tx_orphan_2_no_fee, tx_orphan_2_valid], node, success=False)
+        # Send invalid tx from p2ps[1]
+        node.p2ps[1].send_txs_and_test(
+            [tx_orphan_2_invalid], node, success=False)
+
+        # Mempool should be empty
+        assert_equal(0, node.getmempoolinfo()['size'])
+        assert_equal(2, len(node.getpeerinfo()))  # p2ps[1] is still connected
+
+        self.log.info('Send the withhold tx ... ')
+        node.p2p.send_txs_and_test([tx_withhold], node, success=True)
+
+        # Transactions that should end up in the mempool
+        expected_mempool = {
+            t.hash
+            for t in [
+                tx_withhold,  # The transaction that is the root for all orphans
+                tx_orphan_1,  # The orphan transaction that splits the coins
+                # The valid transaction (with sufficient fee)
+                tx_orphan_2_valid,
+            ]
+        }
+        # Transactions that do not end up in the mempool
+        # tx_orphan_no_fee, because it has too low fee (p2ps[0] is not disconnected for relaying that tx)
+        # tx_orphan_invaid, because it has negative fee (p2ps[1] is disconnected for relaying that tx)
+
+        # p2ps[1] is no longer connected
+        wait_until(lambda: 1 == len(node.getpeerinfo()), timeout=12)
+        assert_equal(expected_mempool, set(node.getrawmempool()))
+
+        # restart node with sending BIP61 messages disabled, check that it disconnects without sending the reject message
+        self.log.info(
+            'Test a transaction that is rejected, with BIP61 disabled')
+        self.restart_node(0, ['-enablebip61=0', '-persistmempool=0'])
+        self.reconnect_p2p(num_connections=1)
+        node.p2p.send_txs_and_test(
+            [tx1], node, success=False, expect_disconnect=True)
+        # send_txs_and_test will have waited for disconnect, so we can safely check that no reject has been received
+        assert_equal(node.p2p.reject_code_received, None)
 
 
 if __name__ == '__main__':
