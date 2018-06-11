fdb7aa180185d0a528223922fba319dbf7a4f403
Wladimir J. van der Laan <laanwj@gmail.com>
2018-06-11 14:08:27 +0200

    Merge #13043: [qt] OptionsDialog: add prune setting
    Summary:
    cbede7dbfde83d53ef38d257e9940af5f163b03c [qt] OptionsDialog: add prune setting (Sjors Provoost)
    
    Pull request description:
    
      The default suggested value is 2 GB. Minimum is 1 GB (550 MB rounded up).
    
      When the user toggles this setting, a strong warning appears that undoing requires re-downloading the chain:
    
      <img width="478" alt="schermafbeelding 2018-05-15 om 12 35 24" src="https://user-images.githubusercontent.com/10217/40051858-7939cc20-583c-11e8-9120-327a75376732.png">
    
      Tooltip points out that actual disk usage can be higher. It's a bit vague on the "advanced features", because I'm assuming anyone who needs to use `-rescan` and `-txindex` will read the documentation, and a more detailed text would needlessly confuse everyone else.
    
      <img width="450" alt="schermafbeelding 2018-05-15 om 12 33 51" src="https://user-images.githubusercontent.com/10217/40051791-49d6156a-583c-11e8-97b9-7de6dfd8c481.png">
    
      The UI uses gigabytes for readability and easy of use. There is also no manual pruning UI (`prune=1`). The user will have to use `bitcoin.conf` for those things.
    
      Fixes #6461. When combined with #13029 the user, after pruning their node, can safely reset settings and/or use bitcoind without having to edit `bitcoin.conf`. However I don't think that's an essential prerequisite.
    
    Tree-SHA512: e17aff276d7235fbd40796adb6431d430620788a753ee13bc064abd35d2edc4280a3d3cddc18e42b4e00edff13ed18fd4f2a966c6f0b43b689afd13673e0c4bf
    
    Backport of Core PR13043
    https://github.com/bitcoin/bitcoin/pull/13043/
    
    Test Plan:
      make check
      bitcoin-qt
      Settings -> Options -> Main tab
    {F4079935}
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4271

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -50,6 +50,8 @@ public:
         Theme,               // QString
         CoinControlFeatures, // bool
         ThreadsScriptVerif,  // int
+        Prune,               // bool
+        PruneSize,           // int
         DatabaseCache,       // int
         SpendZeroConfChange, // bool
         Listen,              // bool
