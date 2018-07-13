7e8aed9188400c9d667fa5f14017dc9463684d25
Jonas Schnelli <dev@jonasschnelli.ch>
2018-07-13 20:34:35 +0100

    Merge #12944: [wallet] ScanforWalletTransactions should mark input txns as dirty
    Summary:
    3c292cc19 ScanforWalletTransactions should mark input txns as dirty (Gregory Sanders)
    
    Pull request description:
    
      I'm hitting a corner case in my mainnet wallet where I load a restore a wallet, call `rescanblockchain` from RPC, and it's "double counting" an output I've sent to myself since currently it never marks input transactions as dirty. This is fixed by a restart of the wallet.
    
      Note that this only happens with keys with birthdate *after* the blocks containing the spent funds which gets scanned on startup, so it's hard to test without a set seed function.
    
    Tree-SHA512: ee1fa152bb054b57ab4c734e355df10d241181e0372c81d583be61678fffbabe5ae60b09b05dc1bbbcfb4838df9d8538791d4c1d80a09b84d78ad2f50dcb0a61
    
    Backport of Core PR12944
    https://github.com/bitcoin/bitcoin/pull/12944/
    
    Test Plan:
      make check
      test_runner.py
    
    Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Reviewed By: jasonbcox, O1 Bitcoin ABC, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D4674

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1376,10 +1376,11 @@ void CWallet::MarkConflicted(const BlockHash &hashBlock, const TxId &txid) {
 }
 
 void CWallet::SyncTransaction(const CTransactionRef &ptx,
-                              const CBlockIndex *pindex, int posInBlock) {
+                              const CBlockIndex *pindex, int posInBlock,
+                              bool update_tx) {
     const CTransaction &tx = *ptx;
 
-    if (!AddToWalletIfInvolvingMe(ptx, pindex, posInBlock, true)) {
+    if (!AddToWalletIfInvolvingMe(ptx, pindex, posInBlock, update_tx)) {
         // Not one of ours
         return;
     }
@@ -1995,8 +1996,8 @@ CBlockIndex *CWallet::ScanForWalletTransactions(
                 }
                 for (size_t posInBlock = 0; posInBlock < block.vtx.size();
                      ++posInBlock) {
-                    AddToWalletIfInvolvingMe(block.vtx[posInBlock], pindex,
-                                             posInBlock, fUpdate);
+                    SyncTransaction(block.vtx[posInBlock], pindex, posInBlock,
+                                    fUpdate);
                 }
             } else {
                 ret = pindex;
