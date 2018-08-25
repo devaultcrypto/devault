63990a52fcc271f72c784ae34a61703324de4860
practicalswift <practicalswift@users.noreply.github.com>
2018-08-26 00:56:09 +0200

    Move cs_main locking annotations from .cpp to .h
    Summary:
    Move `cs_main` locking annotations from `.cpp` to `.h`.
    
    Backport of Bitcoin Core PR14063
    https://github.com/bitcoin/bitcoin/pull/14063
    
    Test Plan:
    Maybe not necessary because are "aesthetic changes", but just in case:
    
    1. Build with Clang in Debug mode:
    
    ```
    CXX=clang++ CC=clang cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug
    ninja check
    ```
    
    2. Verify that the compiler has not emitted a `thread-safety` warning.
    3. Run the node: `./src/bitcoind -regtest`
    4. Verify that text similar to `"Assertion failed: lock ... not held ..."` is not printed on `stderr`.
    
    Reviewers: Fabien, #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D4149

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4873,8 +4873,7 @@ bool CMerkleTx::IsImmatureCoinBase() const {
 }
 
 bool CWalletTx::AcceptToMemoryPool(const Amount nAbsurdFee,
-                                   CValidationState &state)
-    EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
+                                   CValidationState &state) {
     // Quick check to avoid re-setting fInMempool to false
     if (g_mempool.exists(tx->GetId())) {
         return false;
