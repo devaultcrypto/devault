cc2f06876f4a411bcfd89c035d05b3ab3e6117ba
Akio Nakamura <nakamura@dgtechnologies.co.jp>
2019-01-23 10:57:16 +0900

    Prevent mutex lock fail even if --enable-debug
    Summary:
    This PR intends to resolve #15227.
    
    "configure --debug-enabled" enables "#ifdef DEBUG_LOCKORDER".
    Then "lockdata" (in sync.cpp) will be initialized same as other
    static objects.
    
    But unfortunately, lockdata.push_lock() was called before its
    initialization (via initializing signatureCache which is declared
    in script/sigcache.cpp) on macOS.
    
    This PR apply the "Construct On First Use Idiom" to "lockdata"
    to prevent it.
    
    This is a backport of Core PR15233
    
    Depends on D3476
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D3484

--- src/sync.cpp
+++ src/sync.cpp
@@ -68,7 +68,11 @@ struct LockData {
     LockOrders lockorders;
     InvLockOrders invlockorders;
     std::mutex dd_mutex;
-} static lockdata;
+};
+LockData &GetLockData() {
+    static LockData lockdata;
+    return lockdata;
+}
 
 static thread_local LockStack g_lockstack;
 
@@ -107,6 +111,7 @@ potential_deadlock_detected(const std::pair<void *, void *> &mismatch,
 }
 
 static void push_lock(void *c, const CLockLocation &locklocation) {
+    LockData &lockdata = GetLockData();
     std::lock_guard<std::mutex> lock(lockdata.dd_mutex);
 
     g_lockstack.push_back(std::make_pair(c, locklocation));
@@ -177,6 +182,7 @@ void AssertLockNotHeldInternal(const char *pszName, const char *pszFile,
 }
 
 void DeleteLock(void *cs) {
+    LockData &lockdata = GetLockData();
     if (!lockdata.available) {
         // We're already shutting down.
         return;
