77812a01663f1667fcf2488ad4a956f5df2dfd4a
Proteus <pro@proteanx.com>
2019-02-27 12:09:29 -0500

    fix2

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,8 +43,7 @@ const std::string CLIENT_NAME("DeVault Core");
 #endif
 
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
-//! 
-#define GIT_ARCHIVE 1
+//! $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
 #define GIT_COMMIT_ID "$Format:%h$"
 #define GIT_COMMIT_DATE "$Format:%cD$"
