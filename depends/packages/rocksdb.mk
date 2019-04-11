50700a66dfe81afb4bb0e86c4ac1920c0612c57a
Jon Spock <jonspock@protonmail.com>
2019-04-11 22:59:28 +0000

    Attempt rocksdb build
    Add cmake for travis
    
    Set LDFLAGS for bench/rocksdb, put ifneq for darwin/windows selection
    
    Revert ifneq change

--- depends/packages/rocksdb.mk
+++ depends/packages/rocksdb.mk
@@ -3,6 +3,8 @@ $(package)_version=5.18.3
 $(package)_download_path=https://github.com/facebook/rocksdb/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
 $(package)_sha256_hash=7fb6738263d3f2b360d7468cf2ebe333f3109f3ba1ff80115abd145d75287254
+$(package)_build_opts= CC="$($(package)_cc)"
+$(package)_build_opts= CXX="$($(package)_cxx)"
 
 define $(package)_preprocess_cmds
 endef
@@ -14,11 +16,14 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  PORTABLE=1 $(MAKE) static_lib
+	CC=$($(package)_cc) CXX=$($(package)_cxx) cmake . -DCMAKE_SYSTEM_NAME=Windows -DWITH_TESTS=0 -DWITH_GFLAGS=0 -DPORTABLE=1 -DWITH_RUNTIME_DEBUG=0 -DWITH_TOOLS=0
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) INSTALL_PATH=$($(package)_staging_prefix_dir) LD_LIBRARY_PATH=$(host_prefix) install-static
+	$(MAKE) rocksdb
+	mkdir $($(package)_staging_prefix_dir)/lib
+	cp $($(package)_build_dir)/librocksdb.a $($(package)_staging_prefix_dir)/lib/librocksdb.a
+	cp -r $($(package)_build_dir)/include $($(package)_staging_prefix_dir)
 endef
 
 define $(package)_postprocess_cmds
