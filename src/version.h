018b038f7160efe50cc3b172e6f1e46ff25e5141
Jon Spock <jonspock@protonmail.com>
2019-04-12 10:52:39 -0700

    Just use current protocol vesrion as value for all options

--- src/version.h
+++ src/version.h
@@ -14,35 +14,35 @@ static const int PROTOCOL_VERSION = 70015;
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
+static const int GETHEADERS_VERSION = 70015;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 70015;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 70015;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 70015;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this
 //! version
-static const int NO_BLOOM_VERSION = 70011;
+static const int NO_BLOOM_VERSION = 70015;
 
 //! "sendheaders" command and announcing blocks with headers starts with this
 //! version
-static const int SENDHEADERS_VERSION = 70012;
+static const int SENDHEADERS_VERSION = 70015;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this
 //! version
-static const int FEEFILTER_VERSION = 70013;
+static const int FEEFILTER_VERSION = 70015;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70014;
+static const int SHORT_IDS_BLOCKS_VERSION = 70015;
 
 //! not banning for invalid compact blocks starts with this version
 static const int INVALID_CB_NO_BAN_VERSION = 70015;
