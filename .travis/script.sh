452b33cc224a8ba0c3bcbc53c578918dd944c441
Pro X <pro@proteanx.com>
2019-04-13 20:32:31 -0400

    Revert "travis linux builds"
    This reverts commit fc3d7b139a6eca13b2a8d68b03c9e713c80e64df.

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,7 +6,7 @@ TRAVIS_COMMIT_LOG=$(git log --format=fuller -1)
 export TRAVIS_COMMIT_LOG
 
 OUTDIR=$BASE_OUTDIR/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
-BITCOIN_CONFIG_ALL="--disable-bench --disable-tests --with-seeder=false --prefix=$TRAVIS_BUILD_DIR/depends/$HOST --bindir=$OUTDIR/bin --libdir=$OUTDIR/lib"
+BITCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$TRAVIS_BUILD_DIR/depends/$HOST --bindir=$OUTDIR/bin --libdir=$OUTDIR/lib"
 if [ -z "$NO_DEPENDS" ]; then
   DOCKER_EXEC ccache --max-size=$CCACHE_SIZE
 fi
