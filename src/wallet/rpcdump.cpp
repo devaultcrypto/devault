be44cc0a36eba7b94e9160ff31241b9876628f5a
Jon Spock <jonspock@protonmail.com>
2019-04-17 11:13:21 -0700

    Remove warning and dead code

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -32,29 +32,12 @@
 #include <iomanip> // for get_time
 
 int64_t static DecodeDumpTime(const std::string& str) {
-#pragma warning "Need to check as converted from boost, also no locale";
   std::istringstream iss(str);
   std::tm tm = {};
   iss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
   auto tp = std::chrono::system_clock::from_time_t(std::mktime(&tm));
   return std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch()).count();
-  // return (ptime - epoch).total_seconds();
 }
-/*
-static int64_t DecodeDumpTime(const std::string &str) {
-    static const boost::posix_time::ptime epoch =
-        boost::posix_time::from_time_t(0);
-    static const std::locale loc(
-        std::locale::classic(),
-        new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
-    std::istringstream iss(str);
-    iss.imbue(loc);
-    boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
-    iss >> ptime;
-    if (ptime.is_not_a_date_time()) return 0;
-    return (ptime - epoch).total_seconds();
-}
-*/
 
 static std::string EncodeDumpString(const std::string &str) {
     std::stringstream ret;
