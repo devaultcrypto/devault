0be0cfb58315a670597b549dbb415eb5988525e9
Pro X <pro@proteanx.com>
2019-04-28 11:39:21 -0400

    disable log renaming, new icon
    + fix deprecated "topLevelWidget"

--- src/init.cpp
+++ src/init.cpp
@@ -1812,7 +1812,7 @@ bool AppInitMain(Config &config,
 #endif
 
     BCLog::Logger &logger = GetLogger();
-    std::string RenamedLogfile = logger.RenameLastDebugFile();
+//    std::string RenamedLogfile = logger.RenameLastDebugFile();
   
     bool default_shrinkdebugfile = logger.DefaultShrinkDebugFile();
     if (gArgs.GetBoolArg("-shrinkdebugfile", default_shrinkdebugfile)) {
@@ -1827,13 +1827,13 @@ bool AppInitMain(Config &config,
         }
     }
     // Can only remove older after creating new one since there are LogPrintf in there!
-    if (RenamedLogfile != "") LogPrintf("Renamed old log file %s to %s\n",DEFAULT_DEBUGLOGFILE, RenamedLogfile);
+ /*   if (RenamedLogfile != "") LogPrintf("Renamed old log file %s to %s\n",DEFAULT_DEBUGLOGFILE, RenamedLogfile);
     logger.RemoveOlderDebugFiles();
 
 
     if (!logger.m_log_timestamps) {
         LogPrintf("Startup time: %s\n", FormatISO8601DateTime(GetTime()));
-    }
+    } */
     LogPrintf("Default data directory %s\n", GetDefaultDataDir().string());
     LogPrintf("Using data directory %s\n", GetDataDir().string());
     LogPrintf(
@@ -1851,7 +1851,7 @@ bool AppInitMain(Config &config,
                   "'%s'. This is fragile, because if bitcoin is started in the "
                   "future from a different location, it will be unable to "
                   "locate the current data files. There could also be data "
-                  "loss if bitcoin is started while in a temporary "
+                  "loss if devault is started while in a temporary "
                   "directory.\n",
                   gArgs.GetArg("-datadir", ""), fs::current_path().string());
     }
