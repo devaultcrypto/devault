bf8ca9e36ea042143aec56eeb9a51674e42b3ec2
Fabien <fabcien@gmail.com>
2019-05-13 10:09:12 +0000

    [CMAKE] Disable OpenGL in Qt static build for OSX
    Summary:
    A bug in the CMake files distributed by Qt prevent building with static
    OpenGL on OSX (hard linked dependency to the framework in the Qt5Gui
    dependencies). To workaround the issue, the static Qt is built without
    the OpenGL support.
    
    This fixes the `bitcoin-qt` cross build for OSX.
    
    Depends on D3017 and D3033 (for the test plan).
    
    Test Plan:
      mkdir buildcmake && cd buildcmake
      cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/OSX.cmake ..
      ninja
    Run the generated src/qt/bitcoin-qt on OSX >= 10.8 (tested on OSX Sierra
    10.12.6).
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D3034

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -40,6 +40,7 @@ $(package)_config_opts += -no-kms
 $(package)_config_opts += -no-linuxfb
 $(package)_config_opts += -no-libudev
 $(package)_config_opts += -no-mtdev
+$(package)_config_opts += -no-opengl
 $(package)_config_opts += -no-openvg
 $(package)_config_opts += -no-reduce-relocations
 $(package)_config_opts += -no-qml-debug
@@ -101,13 +102,12 @@ $(package)_config_opts_linux += -qt-xcb
 $(package)_config_opts_linux += -system-freetype
 $(package)_config_opts_linux += -no-feature-sessionmanager
 $(package)_config_opts_linux += -fontconfig
-$(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
 $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
-$(package)_config_opts_mingw32  = -no-opengl -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
+$(package)_config_opts_mingw32  = -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
 $(package)_build_env  = QT_RCC_TEST=1
 $(package)_build_env += QT_RCC_SOURCE_DATE_OVERRIDE=1
 endef
