2eda9406d42f0a90858a76c1f95ca39e614e1b72
Pro X <pro@proteanx.com>
2019-06-08 14:55:47 -0400

    Throw error for invalid coin precision

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -141,7 +141,7 @@ Amount AmountFromValue(const UniValue &value) {
     }
 
     int64_t n;
-    if (!ParseFixedPoint(value.getValStr(), 8, &n)) {
+    if (!ParseFixedPoint(value.getValStr(), 3, &n)) {
         throw JSONRPCError(RPC_TYPE_ERROR, "Invalid amount");
     }
 
