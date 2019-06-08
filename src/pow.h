2036be9be4d6187f57caf496c6fcfb411ee3d0b7
Jon Spock <jonspock@protonmail.com>
2019-06-08 16:19:09 -0700

    Add copyrights

--- src/pow.h
+++ src/pow.h
@@ -1,10 +1,10 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
+// Copyright (c) 2019 DeVault developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_POW_H
-#define BITCOIN_POW_H
+#pragma once
 
 #include <cstdint>
 
@@ -22,4 +22,3 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
  */
 bool CheckProofOfWork(uint256 hash, uint32_t nBits, const Config &config);
 
-#endif // BITCOIN_POW_H
