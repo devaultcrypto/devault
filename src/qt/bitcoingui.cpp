bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/rpcconsole.cpp

src/qt/bitcoingui.cpp
@@ -152,7 +152,7 @@ BitcoinGUI::BitcoinGUI(ipc::Node& _ipcNode, const PlatformStyle *_platformStyle,
     setUnifiedTitleAndToolBarOnMac(true);
 #endif
 
-    rpcConsole = new RPCConsole(_platformStyle, 0);
+    rpcConsole = new RPCConsole(ipcNode, _platformStyle, 0);
     helpMessageDialog = new HelpMessageDialog(ipcNode, this, false);
 #ifdef ENABLE_WALLET
     if(enableWallet)

devaultcrypto/devault

Summary:
Partial backport of Core PR 10244
https://github.com/bitcoin/bitcoin/pull/10244/commits/582daf6d22da5394d02a12003b9542d9f5865ae2
Progress towards T417
Depends on D2822
Test Plan:
make check
ninja check
bitcoin-qt # sanity checks + ban table renders as expected
Reviewers: deadalnix, Fabien, #bitcoin_abc
Reviewed By: Fabien, #bitcoin_abc
Differential Revision: https://reviews.bitcoinabc.org/D2845

src/qt/bitcoingui.cpp
@@ -118,8 +118,8 @@ BitcoinGUI::BitcoinGUI(interfaces::Node &node, const Config *configIn,
 #endif
     setWindowTitle(windowTitle);
 
-    rpcConsole = new RPCConsole(_platformStyle, nullptr);
-    helpMessageDialog = new HelpMessageDialog(m_node, this, false);
+    rpcConsole = new RPCConsole(node, _platformStyle, 0);
+    helpMessageDialog = new HelpMessageDialog(node, this, false);
     if (enableWallet) {
         /** Create wallet frame and make it the central widget */
         walletFrame = new WalletFrame(_platformStyle, config, this);