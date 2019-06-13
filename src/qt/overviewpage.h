bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Use WalletBalances struct in Qt
Suggested by John Newbery <john@johnnewbery.com>
https://github.com/bitcoin/bitcoin/pull/10244#discussion_r177504284

src/qt/overviewpage.h
@@ -5,7 +5,7 @@
 #ifndef BITCOIN_QT_OVERVIEWPAGE_H
 #define BITCOIN_QT_OVERVIEWPAGE_H
 
-#include <amount.h>
+#include <interface/wallet.h>
 
 #include <QWidget>
 #include <memory>
@@ -38,8 +38,7 @@ public:
     void showOutOfSyncWarning(bool fShow);
 
 public Q_SLOTS:
-    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
-                    const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
+    void setBalance(const interface::WalletBalances& balances);
 
 Q_SIGNALS:
     void transactionClicked(const QModelIndex &index);
@@ -49,12 +48,7 @@ private:
     Ui::OverviewPage *ui;
     ClientModel *clientModel;
     WalletModel *walletModel;
-    CAmount currentBalance;
-    CAmount currentUnconfirmedBalance;
-    CAmount currentImmatureBalance;
-    CAmount currentWatchOnlyBalance;
-    CAmount currentWatchUnconfBalance;
-    CAmount currentWatchImmatureBalance;
+    interface::WalletBalances m_balances;
 
     TxViewDelegate *txdelegate;
     std::unique_ptr<TransactionFilterProxy> filter;

src/qt/walletmodel.h
@@ -237,8 +237,7 @@ private:
 
 Q_SIGNALS:
     // Signal that balance in wallet changed
-    void balanceChanged(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
-                        const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
+    void balanceChanged(const interface::WalletBalances& balances);
 
     // Encryption status of wallet changed
     void encryptionStatusChanged();

devaultcrypto/devault

Summary:
Partial backport of Core PR 10244
https://github.com/bitcoin/bitcoin/pull/10244/commits/9a61eed1fcc16ddcedc315045f470e1958b0760b
Progress towards T417
Depends on D2889
Test Plan:
make check
ninja check
bitcoin-qt # sanity checks
Reviewers: deadalnix, Fabien, #bitcoin_abc
Reviewed By: Fabien, #bitcoin_abc
Differential Revision: https://reviews.bitcoinabc.org/D2899

src/qt/overviewpage.h
@@ -9,12 +9,7 @@
 #include "transactionview.h"
 
 
-#ifdef HAVE_VARIANT
-#include <optional>
-#else
-#include <boost/optional.hpp>
-#endif
-
+#include <interfaces/wallet.h>
 
 #include <QWidget>
 #include <memory>
@@ -49,10 +44,7 @@ public:
     TransactionView *transactionView;
 
 public Q_SLOTS:
-    void setBalance(const Amount balance, const Amount unconfirmedBalance,
-                    const Amount immatureBalance, const Amount watchOnlyBalance,
-                    const Amount watchUnconfBalance,
-                    const Amount watchImmatureBalance);
+    void setBalance(const interfaces::WalletBalances &balances);
 
 Q_SIGNALS:
  //   void transactionClicked(const QModelIndex &index);
@@ -62,17 +54,7 @@ private:
     Ui::OverviewPage *ui;
     ClientModel *clientModel;
     WalletModel *walletModel;
-#ifdef HAVE_VARIANT  
-    std::optional<Amount> currentBalanceOptional;
-#else
-    boost::optional<Amount> currentBalanceOptional;
-#endif
-    Amount currentBalance;
-    Amount currentUnconfirmedBalance;
-    Amount currentImmatureBalance;
-    Amount currentWatchOnlyBalance;
-    Amount currentWatchUnconfBalance;
-    Amount currentWatchImmatureBalance;
+    interfaces::WalletBalances m_balances;
 
     TxViewDelegate *txdelegate;
     std::unique_ptr<TransactionFilterProxy> filter;

src/qt/walletmodel.h
@@ -226,11 +226,7 @@ private:
 
 Q_SIGNALS:
     // Signal that balance in wallet changed
-    void balanceChanged(const Amount balance, const Amount unconfirmedBalance,
-                        const Amount immatureBalance,
-                        const Amount watchOnlyBalance,
-                        const Amount watchUnconfBalance,
-                        const Amount watchImmatureBalance);
+    void balanceChanged(const interfaces::WalletBalances &balances);
 
     // Encryption status of wallet changed
     void encryptionStatusChanged();