13987d317e9b030fb79da9c8cf9a7e81297afbf0
Jon Spock <jonspock@protonmail.com>
2019-06-15 13:06:39 -0700

    Tighten up unit tests + accuracy for rewards

--- src/devault/rewards_calculation.cpp
+++ src/devault/rewards_calculation.cpp
@@ -15,10 +15,10 @@ Amount CalculateReward(const Consensus::Params &consensusParams, int Height, int
   int64_t percent = consensusParams.nPerCentPerYear[year_number];
   int64_t nRewardRatePerBlockReciprocal = (100*consensusParams.nBlocksPerYear)/percent;
   Amount nMinReward = consensusParams.nMinReward;
-  Amount reward_per_block = balance / nRewardRatePerBlockReciprocal;
-  Amount reward = HeightDiff * reward_per_block;
+  int64_t reward_per_block_x100 = ((100*balance.toInt()) / nRewardRatePerBlockReciprocal);
+  int64_t reward_x100 = (HeightDiff * reward_per_block_x100) / Amount::COIN_PRECISION;
   // Quantize reward to 1/100th of a coin
-  reward = (100 * reward).toIntCoins() * (COIN / 100);
+  Amount reward = reward_x100 * (COIN / 100);
 
   // double debug_reward_div = OverRewardRatePerBlock * HeightDiff;
   if (reward < nMinReward) reward = Amount();
--- src/test/reward_tests.cpp
+++ src/test/reward_tests.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-
 #include "chainparams.h"
 #include "config.h"
 
@@ -11,30 +10,72 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <array>
+#include <vector>
 
 BOOST_FIXTURE_TEST_SUITE(reward_tests, BasicTestingSetup)
 
-BOOST_AUTO_TEST_CASE(RewardTests) {
+// For 1st year below 4000 shouldn't payoff before 1 month
+BOOST_AUTO_TEST_CASE(zero_reward_checks) {
     DummyConfig config(CBaseChainParams::MAIN);
-    std::array<int64_t, 8> months = {{1,2,6,6,6,24,48, 60}};
-    std::array<int64_t, 8> balances = {{4000, 40000, 400000, 4000000, 5000, 10000, 50000, 99999999}};
-    std::array<int64_t, 8> min_balances = {{1,1,1,1,1,1,1,1}};
-    std::array<int64_t, 8> ref_amount = {{65,0,65,0,65,65,635, 624976}};
+    std::vector<int64_t> balances = {3999, 1999, 999};
+    std::vector<int64_t> height_diff_months = {1, 2, 4};
 
     const Consensus::Params &params = config.GetChainParams().GetConsensus();
+    int month = params.nMinRewardBlocks;
 
-  
-    for (int i=0;i<8;i++) {
-      // 1 full month (for now)
-      int height_diff = params.nMinRewardBlocks;
-      int height = months[i]*params.nMinRewardBlocks;
-      Amount bal(balances[i] * COIN);
-      Amount r(ref_amount[i]*Amount::COIN_PRECISION);
+    int i = 0;
+    for (auto balance : balances) {
+        int height_diff = (height_diff_months[i] * month +
+                           1); // 1 extra block because of rounding down
+        int height = 0;        // 1st year
+        Amount bal(balance * COIN);
+        Amount v(CalculateReward(params, height, height_diff, bal));
+        BOOST_CHECK_EQUAL(v, Amount::zero());
+        i++;
+    }
+}
 
-      Amount v(CalculateReward(params, height, height_diff, bal));
-      std::cout << " bal = " << bal.ToString() << " for " << months[i] << "m : " << v.ToString() << "\n";
-      // BOOST_CHECK_EQUAL(CalculateReward(params, height_diff, height_diff, bal), r);
+// 12000 should payout decreasing amounts down to min of 50 after 5 years
+BOOST_AUTO_TEST_CASE(Reward12k) {
+    DummyConfig config(CBaseChainParams::MAIN);
+    std::vector<int64_t> years = {0, 1, 2, 3, 4, 5};
+    std::vector<int64_t> ref_amount = {150, 120, 90, 70, 50, 50};
+
+    const Consensus::Params &params = config.GetChainParams().GetConsensus();
+    int month = params.nMinRewardBlocks + 1;
+
+    int i = 0;
+    Amount bal(12000 * COIN);
+    int height_diff = month;
+
+    for (auto y : years) {
+        int height = y * 12 * month;
+        Amount r(ref_amount[i] * Amount::COIN_PRECISION);
+        Amount v(CalculateReward(params, height, height_diff, bal));
+        BOOST_CHECK_EQUAL(v, r);
+        i++;
+    }
+}
+
+// Amount Balances needed to get Minimum Reward Amount
+BOOST_AUTO_TEST_CASE(minRewardTests) {
+    DummyConfig config(CBaseChainParams::MAIN);
+    std::vector<int64_t> balances = {4000, 5000, 6667, 8572, 12000};
+    std::vector<int64_t> years = {0, 1, 2, 3, 4, 5};
+
+    const Consensus::Params &params = config.GetChainParams().GetConsensus();
+    int month = params.nMinRewardBlocks + 1;
+    int height_diff = month;
+
+    int i = 0;
+    for (auto balance : balances) {
+        int height = years[i] * 12 * month;
+        Amount bal(balance * COIN);
+        Amount v(CalculateReward(params, height, height_diff, bal));
+        // std::cout << " bal = " << bal.ToString() << " @ " << years[i] << "
+        // years: " << v.ToString() << "\n";
+        BOOST_CHECK_EQUAL(v, params.nMinReward);
+        i++;
     }
 }
 
