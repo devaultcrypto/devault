bff6455a0ff3db04d3b31d25871eb6bd973ca9b0
Jon Spock <jonspock@protonmail.com>
2019-06-21 19:36:47 -0700

    Add a class to allow timing code/functions

---
+++ src/benchmark.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2019 The DeVault developers
+// Copyright (c) 2019 Jon Spock
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+// For micro-benchmarking some tasks/code
+
+#pragma once
+#include <sys/time.h>
+class Benchmark
+{
+	timeval timer[2];
+
+public:
+
+	timeval start()
+	{
+		gettimeofday(&this->timer[0], NULL);
+		return this->timer[0];
+	}
+
+	timeval stop()
+	{
+		gettimeofday(&this->timer[1], NULL);
+		return this->timer[1];
+	}
+
+	int uduration() const
+	{
+		int secs(this->timer[1].tv_sec - this->timer[0].tv_sec);
+		int usecs(this->timer[1].tv_usec - this->timer[0].tv_usec);
+
+		if(usecs < 0)
+		{
+			--secs;
+			usecs += 1000000;
+		}
+
+		return (usecs);
+	}
+};
+
