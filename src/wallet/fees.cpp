Bitcoin-ABC/bitcoin-abc

Rework the wallet fees interface to make it closer to core's
Summary: All of this was fubared for no good reason.

Test Plan:
  make check

Reviewers: #bitcoin_abc, jasonbcox

Reviewed By: #bitcoin_abc, jasonbcox

Subscribers: jasonbcox

Differential Revision: https://reviews.bitcoinabc.org/D3233

src/wallet/fees.cpp
@@ -13,8 +13,16 @@
 #include <wallet/coincontrol.h>
 #include <wallet/wallet.h>
 
-static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
-                            Amount targetFee) {
+Amount GetRequiredFee(unsigned int nTxBytes) {
+    return GetConfig().GetMinFeePerKB().GetFee(nTxBytes);
+}
+
+Amount GetMinimumFee(unsigned int nTxBytes, const CCoinControl &coinControl,
+                     const CTxMemPool &pool) {
+    Amount targetFee = (coinControl.fOverrideFeeRate && coinControl.m_feerate)
+                           ? coinControl.m_feerate->GetFee(nTxBytes)
+                           : payTxFee.GetFeeCeiling(nTxBytes);
+
     Amount nFeeNeeded = targetFee;
     if (nFeeNeeded == Amount::zero()) {
         nFeeNeeded = pool.estimateFee().GetFeeCeiling(nTxBytes);
@@ -26,8 +34,7 @@ static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
     }
 
     // Prevent user from paying a fee below minRelayTxFee or minTxFee.
-    nFeeNeeded =
-        std::max(nFeeNeeded, GetConfig().GetMinFeePerKB().GetFee(nTxBytes));
+    nFeeNeeded = std::max(nFeeNeeded, GetRequiredFee(nTxBytes));
 
     // But always obey the maximum.
     if (nFeeNeeded > maxTxFee) {
@@ -35,19 +42,4 @@ static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
     }
 
     return nFeeNeeded;
-}
-
-Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool) {
-    // payTxFee is the user-set global for desired feerate.
-    return GetMinimumFee(nTxBytes, pool, payTxFee.GetFeeCeiling(nTxBytes));
-}
-
-Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
-                     const CCoinControl &coinControl) {
-    if (coinControl.fOverrideFeeRate && coinControl.m_feerate) {
-        return GetMinimumFee(nTxBytes, pool,
-                             coinControl.m_feerate->GetFee(nTxBytes));
-    } else {
-        return GetMinimumFee(nTxBytes, pool);
-    }
 }

devaultcrypto/devault

src/wallet/fees.cpp
@@ -14,8 +14,16 @@
 #include <wallet/coincontrol.h>
 #include <wallet/wallet.h>
 
-static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
-                            Amount targetFee) {
+Amount GetRequiredFee(unsigned int nTxBytes) {
+    return GetConfig().GetMinFeePerKB().GetFee(nTxBytes);
+}
+
+Amount GetMinimumFee(unsigned int nTxBytes, const CCoinControl &coinControl,
+                     const CTxMemPool &pool) {
+    Amount targetFee = (coinControl.fOverrideFeeRate && coinControl.m_feerate)
+                           ? coinControl.m_feerate->GetFee(nTxBytes)
+                           : payTxFee.GetFee(nTxBytes);
+
     Amount nFeeNeeded = targetFee;
     if (nFeeNeeded == Amount::zero()) {
         nFeeNeeded = pool.estimateFee().GetFee(nTxBytes);
@@ -27,8 +35,7 @@ static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
     }
 
     // Prevent user from paying a fee below minRelayTxFee or minTxFee.
-    nFeeNeeded =
-        std::max(nFeeNeeded, GetConfig().GetMinFeePerKB().GetFee(nTxBytes));
+    nFeeNeeded = std::max(nFeeNeeded, GetRequiredFee(nTxBytes));
 
     // But always obey the maximum.
     if (nFeeNeeded > maxTxFee) {
@@ -36,19 +43,4 @@ static Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
     }
 
     return nFeeNeeded;
-}
-
-Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool) {
-    // payTxFee is the user-set global for desired feerate.
-    return GetMinimumFee(nTxBytes, pool, payTxFee.GetFee(nTxBytes));
-}
-
-Amount GetMinimumFee(unsigned int nTxBytes, const CTxMemPool &pool,
-                     const CCoinControl &coinControl) {
-    if (coinControl.fOverrideFeeRate && coinControl.m_feerate) {
-        return GetMinimumFee(nTxBytes, pool,
-                             coinControl.m_feerate->GetFee(nTxBytes));
-    } else {
-        return GetMinimumFee(nTxBytes, pool);
-    }
 }