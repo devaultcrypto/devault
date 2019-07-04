b59d232de02fa0940de89a96c4f06225df034646
Jon Spock <jonspock@protonmail.com>
2019-07-04 16:35:09 -0700

    remove unused interfaces, update comments

--- src/consensus/activation.h
+++ src/consensus/activation.h
@@ -1,25 +1,13 @@
 // Copyright (c) 2018 The Bitcoin developers
+// Copyright (c) 2019 The DeVault developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CONSENSUS_ACTIVATION_H
-#define BITCOIN_CONSENSUS_ACTIVATION_H
-
-#include <cstdint>
+#pragma once
 
 class CBlockIndex;
 class Config;
 
-/** Check if UAHF has activated. */
-bool IsUAHFenabled(const Config &config, const CBlockIndex *pindexPrev);
-
-/** Check if Nov 15, 2018 HF has activated using block height. */
-bool IsMagneticAnomalyEnabled(const Config &config, int32_t nHeight);
-/** Check if Nov 15, 2018 HF has activated using previous block index. */
-bool IsMagneticAnomalyEnabled(const Config &config,
-                              const CBlockIndex *pindexPrev);
-
-/** Check if May 15th, 2019 protocol upgrade has activated. */
+/** Check Schnorr related protocol upgrade has activated. */
 bool IsGreatWallEnabled(const Config &config, const CBlockIndex *pindexPrev);
 
-#endif // DEVAULT_CONSENSUS_ACTIVATION_H
