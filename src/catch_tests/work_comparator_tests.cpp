e5696bb48d11c82fde53cdac77fc8e4b7325f6a6
Fabien <fabcien@gmail.com>
2019-07-05 20:02:11 +0200

    Fix memory leak in work_comparator_tests.cpp
    Summary: The CBlockIndex are never freed.
    
    Test Plan:
    Build with asan, then:
      ./src/test/test_bitcoin -t work_comparator_tests
    
    Reviewers: #bitcoin_abc, deadalnix, jasonbcox
    
    Reviewed By: #bitcoin_abc, jasonbcox
    
    Differential Revision: https://reviews.bitcoinabc.org/D3568

--- src/catch_tests/work_comparator_tests.cpp
+++ src/catch_tests/work_comparator_tests.cpp
@@ -8,49 +8,51 @@
 
 // BOOST_AUTO_TEST_SUITE(work_comparator_tests)
 
-TEST_CASE("work_comparator") {
-  CBlockIndex indexA, indexB;
+BOOST_AUTO_TEST_CASE(work_comparator) {
+    CBlockIndex indexA, indexB;
 
-  // Differing chain work
-  indexA.nChainWork = 0;
-  indexB.nChainWork = 1;
-  for (int sequenceIdA = 1; sequenceIdA < 1024; sequenceIdA *= 2) {
-    for (int sequenceIdB = 1; sequenceIdB < 1024; sequenceIdB *= 2) {
-      // Differing sequenceId doesn't affect chain work
-      indexA.nSequenceId = sequenceIdA;
-      indexB.nSequenceId = sequenceIdB;
-      BOOST_CHECK(CBlockIndexWorkComparator()(&indexA, &indexB));
+    // Differing chain work
+    indexA.nChainWork = 0;
+    indexB.nChainWork = 1;
+    for (int sequenceIdA = 1; sequenceIdA < 1024; sequenceIdA *= 2) {
+        for (int sequenceIdB = 1; sequenceIdB < 1024; sequenceIdB *= 2) {
+            // Differing sequenceId doesn't affect chain work
+            indexA.nSequenceId = sequenceIdA;
+            indexB.nSequenceId = sequenceIdB;
+            BOOST_CHECK(CBlockIndexWorkComparator()(&indexA, &indexB));
+        }
     }
-  }
 
-  // Same chain work, but differing sequenceId
-  indexA = CBlockIndex();
-  indexB = CBlockIndex();
-  for (int sequenceIdA = 1; sequenceIdA < 1024; sequenceIdA *= 2) {
-    for (int sequenceIdB = 1; sequenceIdB < 1024; sequenceIdB *= 2) {
-      if (sequenceIdA == sequenceIdB) { continue; }
+    // Same chain work, but differing sequenceId
+    indexA = CBlockIndex();
+    indexB = CBlockIndex();
+    for (int sequenceIdA = 1; sequenceIdA < 1024; sequenceIdA *= 2) {
+        for (int sequenceIdB = 1; sequenceIdB < 1024; sequenceIdB *= 2) {
+            if (sequenceIdA == sequenceIdB) {
+                continue;
+            }
 
-      indexA.nSequenceId = sequenceIdA;
-      indexB.nSequenceId = sequenceIdB;
-      if (sequenceIdA > sequenceIdB) {
-        BOOST_CHECK(CBlockIndexWorkComparator()(&indexA, &indexB));
-      } else {
-        BOOST_CHECK(CBlockIndexWorkComparator()(&indexB, &indexA));
-      }
+            indexA.nSequenceId = sequenceIdA;
+            indexB.nSequenceId = sequenceIdB;
+            if (sequenceIdA > sequenceIdB) {
+                BOOST_CHECK(CBlockIndexWorkComparator()(&indexA, &indexB));
+            } else {
+                BOOST_CHECK(CBlockIndexWorkComparator()(&indexB, &indexA));
+            }
+        }
     }
-  }
 
-  // All else equal, so checking pointer address as final check
-  CBlockIndex *pindexA = new CBlockIndex();
-  CBlockIndex *pindexB = new CBlockIndex();
-  if (pindexA < pindexB) {
-    BOOST_CHECK(CBlockIndexWorkComparator()(pindexB, pindexA));
-  } else {
-    BOOST_CHECK(CBlockIndexWorkComparator()(pindexA, pindexB));
-  }
+    // All else equal, so checking pointer address as final check
+    std::unique_ptr<CBlockIndex> pindexA(new CBlockIndex());
+    std::unique_ptr<CBlockIndex> pindexB(new CBlockIndex());
+    if (pindexA < pindexB) {
+        BOOST_CHECK(CBlockIndexWorkComparator()(pindexB.get(), pindexA.get()));
+    } else {
+        BOOST_CHECK(CBlockIndexWorkComparator()(pindexA.get(), pindexB.get()));
+    }
 
-  // Same block should return false
-  BOOST_CHECK(!CBlockIndexWorkComparator()(pindexA, pindexA));
+    // Same block should return false
+    BOOST_CHECK(!CBlockIndexWorkComparator()(pindexA.get(), pindexA.get()));
 }
 
-// BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
