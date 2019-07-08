3a16741be40378d6bb3ad1e259991b34fabe8a02
Jon Spock <jonspock@protonmail.com>
2019-07-07 18:11:17 -0700

    Re-enable/fix checkpoint tests

--- src/catch_tests/checkpoints_tests.cpp
+++ src/catch_tests/checkpoints_tests.cpp
@@ -23,25 +23,25 @@
 
 #include "catch_unit.h"
 
-#warning "Checkpoints tests disabled until we have some"
-#ifdef HAVE_CHECKPOINTS
 BOOST_AUTO_TEST_CASE(sanity) {
   const auto params = CreateChainParams(CBaseChainParams::MAIN);
   const CCheckpointData &checkpoints = params->Checkpoints();
-  uint256 p11111 = uint256S("0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d");
-  uint256 p134444 = uint256S("00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe");
-  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 11111, p11111));
-  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 134444, p134444));
+  uint256 p5000 = uint256S("000000000000000173c13a23fed27056b5a76912a27d62064cb988db13888907");
+  uint256 p0 = uint256S("0000000038e62464371566f6a8d35c01aa54a7da351b2dbf85d92f30357f3a90");
+  uint256 p1 = uint256S("173c13a23fed27056b5a76912a27d62064cb988");
+  uint256 p2 = uint256S("38e62464371566f6a8d35c01aa54a7da351b2dbf85d92f");
+
+  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 5000, p5000));
+  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 0, p0));
 
   // Wrong hashes at checkpoints should fail:
-  BOOST_CHECK(!Checkpoints::CheckBlock(checkpoints, 11111, p134444));
-  BOOST_CHECK(!Checkpoints::CheckBlock(checkpoints, 134444, p11111));
+  BOOST_CHECK(!Checkpoints::CheckBlock(checkpoints, 5000, p1));
+  BOOST_CHECK(!Checkpoints::CheckBlock(checkpoints, 0, p2));
 
   // ... but any hash not at a checkpoint should succeed:
-  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 11111 + 1, p134444));
-  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 134444 + 1, p11111));
+  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 1, p1));
+  BOOST_CHECK(Checkpoints::CheckBlock(checkpoints, 5000 + 1, p2));
 }
-#endif
 
 TEST_CASE("ban_fork_at_genesis_block") {
   DummyConfig config;
@@ -94,16 +94,10 @@ class MainnetConfigWithTestCheckpoints : public DummyConfig {
 };
 
 /**
- * This test has 4 precomputed blocks mined ontop of the genesis block:
- *  G ---> A ---> AA (checkpointed)
- *   \       \
- *    \--> B  \-> AB
- * After the node has accepted only A and AA, these rejects should occur:
- *  * B should be rejected for forking prior to an accepted checkpoint
- *  * AB should be rejected for forking at an accepted checkpoint
+ * This test has first 4 blocks mined ontop of the genesis block:
+ * checkpoint is wrong
  */
 
-#ifdef DEBUG_THIS
 TEST_CASE("ban_fork_prior_to_and_at_checkpoints") {
   MainnetConfigWithTestCheckpoints config;
 
@@ -113,78 +107,63 @@ TEST_CASE("ban_fork_prior_to_and_at_checkpoints") {
 
   // Start with mainnet genesis block
   CBlockHeader headerG = config.GetChainParams().GenesisBlock();
-  BOOST_CHECK(headerG.GetHash() == uint256S("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f"
-                                            "1b60a8ce26f"));
+  BOOST_CHECK(headerG.GetHash() == uint256S("0000000038e62464371566f6a8d35c01aa54a7da351b2dbf85d92f30357f3a90"));
   BOOST_CHECK(ProcessNewBlockHeaders(config, {headerG}, state, &pindex, &invalid));
   pindex = nullptr;
 
   CBlockHeader headerA, headerB, headerAA, headerAB;
-  CDataStream stream = CDataStream(ParseHex("010000006fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000"
-                                            "000000982051fd1e4ba744bbbe680e1fee14677ba1a3c3540bf7b1cdb606e85723"
-                                            "3e0e61bc6649ffff001d01e3629901010000000100000000000000000000000000"
-                                            "00000000000000000000000000000000000000ffffffff0704ffff001d0104ffff"
-                                            "ffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390"
-                                            "813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166"
-                                            "bf621e73a82cbf2342c858eeac00000000"),
-                                   SER_NETWORK, PROTOCOL_VERSION);
+  CDataStream stream = CDataStream(
+      ParseHex("00000020903a7f35302fd985bf2d1b35daa754aa015cd3a8f66615376424e63800000000a1f0872be30c0b6c625b5b613ad85d5"
+               "64ecf73dfd7c911e143f2c17a95f95d86b28ff65cffff001d1caf48790101000000010000000000000000000000000000000000"
+               "000000000000000000000000000000ffffffff21010100feccfe0c00fe9d390e000963676d696e6572343208000000000000000"
+               "000ffffffff0100743ba40b0000001976a91480d057b26c1768ef56420a5c752ca7d3ed4a378288ac00000000"),
+
+      SER_NETWORK, PROTOCOL_VERSION);
   stream >> headerA;
-  BOOST_CHECK(headerA.GetHash() == uint256S("00000000839a8e6886ab5951d76f411475428afc90947ee320161"
-                                            "bbf18eb6048"));
+  BOOST_CHECK(headerA.GetHash() == uint256S("00000000a2daf649e9d0c0453caba957f02b441bae82117627a723f399818c9e"));
   BOOST_CHECK(headerA.hashPrevBlock == headerG.GetHash());
 
-  stream = CDataStream(ParseHex("010000004860eb18bf1b1620e37e9490fc8a427514416fd75159ab86688e9a8300"
-                                "000000d5fdcc541e25de1c7a5addedf24858b8bb665c9f36ef744ee42c316022c9"
-                                "0f9bb0bc6649ffff001d08d2bd6101010000000100000000000000000000000000"
-                                "00000000000000000000000000000000000000ffffffff0704ffff001d010bffff"
-                                "ffff0100f2052a010000004341047211a824f55b505228e4c3d5194c1fcfaa15a4"
-                                "56abdf37f9b9d97a4040afc073dee6c89064984f03385237d92167c13e236446b4"
-                                "17ab79a0fcae412ae3316b77ac00000000"),
-                       SER_NETWORK, PROTOCOL_VERSION);
+  stream = CDataStream(
+      ParseHex("000000209e8c8199f323a727761182ae1b442bf057a9ab3c45c0d0e949f6daa200000000f410091944a3c14563ff204c7731cb1"
+               "fbc15cc748ec96f847a4cfbdd0372c2c1b68ff65cffff001d45270cfe0101000000010000000000000000000000000000000000"
+               "000000000000000000000000000000ffffffff21010200fed1fe0c00fed2d306000963676d696e6572343208010000000000000"
+               "000ffffffff0100743ba40b0000001976a91480d057b26c1768ef56420a5c752ca7d3ed4a378288ac00000000"),
+      SER_NETWORK, PROTOCOL_VERSION);
   stream >> headerAA;
-  BOOST_CHECK(headerAA.GetHash() == uint256S("000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da"
-                                             "3fdcc99ddbd"));
+  BOOST_CHECK(headerAA.GetHash() == uint256S("00000000cff2a1fe12fb1ce2c85da0acb2a4a135935fe0a6df26626980770c8f"));
   BOOST_CHECK(headerAA.hashPrevBlock == headerA.GetHash());
 
-  stream = CDataStream(ParseHex("000000206fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000"
-                                "000000bff4e0fd76ec3e9c8853811dec34dda8d5debb24d4113d94235fd4b24bb2"
-                                "92981b70995cffff001d4e6e050001020000000100000000000000000000000000"
-                                "00000000000000000000000000000000000000ffffffff0d51026302082f454233"
-                                "322e302fffffffff0100f2052a01000000232103c91f2fa16c94c92d08629eeb8f"
-                                "d681658d49f2b3016b13336d67d79f858dbc71ac000000001"),
-                       SER_NETWORK, PROTOCOL_VERSION);
+  stream = CDataStream(
+      ParseHex("000000208f0c7780696226dfa6e05f9335a1a4b2aca05dc8e21cfb12fea1f2cf00000000b32819c89b3ad00d447543a10c52cb0"
+               "4032b365711992cd69369c79e4fcec701ba8ff65cffff001d18f22dbf0101000000010000000000000000000000000000000000"
+               "000000000000000000000000000000ffffffff21010300fed4fe0c00fe4b920e000963676d696e6572343208000000000000000"
+               "000ffffffff0100743ba40b0000001976a91480d057b26c1768ef56420a5c752ca7d3ed4a378288ac00000000"),
+      SER_NETWORK, PROTOCOL_VERSION);
   stream >> headerB;
-  BOOST_CHECK(headerB.hashPrevBlock == headerG.GetHash());
+  BOOST_CHECK(headerB.hashPrevBlock == headerAA.GetHash());
 
-  stream = CDataStream(ParseHex("000000204860eb18bf1b1620e37e9490fc8a427514416fd75159ab86688e9a8300"
-                                "0000003800b1dd09f3f1a1c9e62ce8dca6d1e6caacc9a02d178ef6ad95527b49ff"
-                                "863f8282995cffff001d2cc70f0001020000000100000000000000000000000000"
-                                "00000000000000000000000000000000000000ffffffff0d52024902082f454233"
-                                "322e302fffffffff0100f2052a010000002321020a56690eb0e2454c1f362d3599"
-                                "89198a0b23505578be4164a65521ee7751eb1dac00000000"),
-                       SER_NETWORK, PROTOCOL_VERSION);
+  stream = CDataStream(
+      ParseHex("00000020d4b1d0f18751ff1f3d34780e972a2e7adcefef5cfa6658bc78b887430000000008687b9c35cab2e933bfb406dfec5df"
+               "9a39fb4ac8fbd862087538a4e85dd68b4bb8ff65cffff001d63e9f9f80101000000010000000000000000000000000000000000"
+               "000000000000000000000000000000ffffffff21010400fed6fe0c00fea6fd06000963676d696e6572343208010000000000000"
+               "000ffffffff0100743ba40b0000001976a91480d057b26c1768ef56420a5c752ca7d3ed4a378288ac00000000"),
+      SER_NETWORK, PROTOCOL_VERSION);
   stream >> headerAB;
-  BOOST_CHECK(headerAB.hashPrevBlock == headerA.GetHash());
+  BOOST_CHECK(headerAB.hashPrevBlock == headerB.GetHash());
 
-  // Headers A and AA should be accepted
+  // Headers A should be accepted
   BOOST_CHECK(ProcessNewBlockHeaders(config, {headerA}, state, &pindex, &invalid));
   BOOST_CHECK(state.IsValid());
   BOOST_CHECK(pindex != nullptr);
   pindex = nullptr;
   BOOST_CHECK(invalid.IsNull());
 
-  BOOST_CHECK(ProcessNewBlockHeaders(config, {headerAA}, state, &pindex, &invalid));
-  BOOST_CHECK(state.IsValid());
-  BOOST_CHECK(pindex != nullptr);
-  pindex = nullptr;
-  BOOST_CHECK(invalid.IsNull());
-
-  // Header B should be rejected
-  BOOST_CHECK(!ProcessNewBlockHeaders(config, {headerB}, state, &pindex, &invalid));
+  // Header AA should be rejected - checkpoint mismatch
+  BOOST_CHECK(!ProcessNewBlockHeaders(config, {headerAA}, state, &pindex, &invalid));
   BOOST_CHECK(state.IsInvalid());
   BOOST_CHECK(state.GetRejectCode() == REJECT_CHECKPOINT);
-  BOOST_CHECK(state.GetRejectReason() == "bad-fork-prior-to-checkpoint");
+  BOOST_CHECK(state.GetRejectReason() == "checkpoint mismatch");
   BOOST_CHECK(pindex == nullptr);
-  BOOST_CHECK(invalid.GetHash() == headerB.GetHash());
 
   // Sanity check to ensure header was not saved in memory
   {
@@ -192,11 +171,11 @@ TEST_CASE("ban_fork_prior_to_and_at_checkpoints") {
     BOOST_CHECK(LookupBlockIndex(headerB.GetHash()) == nullptr);
   }
 
-  // Header AB should be rejected
+  // Header AB should be rejected since checkpoint mismatch means prev block not stored
   BOOST_CHECK(!ProcessNewBlockHeaders(config, {headerAB}, state, &pindex, &invalid));
   BOOST_CHECK(state.IsInvalid());
-  BOOST_CHECK(state.GetRejectCode() == REJECT_CHECKPOINT);
-  BOOST_CHECK(state.GetRejectReason() == "checkpoint mismatch");
+  BOOST_CHECK(state.GetRejectCode() == 0);
+  BOOST_CHECK(state.GetRejectReason() == "prev-blk-not-found");
   BOOST_CHECK(pindex == nullptr);
   BOOST_CHECK(invalid.GetHash() == headerAB.GetHash());
 
@@ -206,6 +185,5 @@ TEST_CASE("ban_fork_prior_to_and_at_checkpoints") {
     BOOST_CHECK(LookupBlockIndex(headerAB.GetHash()) == nullptr);
   }
 }
-#endif
 
-//BOOST_AUTO_TEST_SUITE_END()
+// BOOST_AUTO_TEST_SUITE_END()
