1ab9643315252daa53562394eca288927cd0d581
Jon Spock <jonspock@protonmail.com>
2019-07-28 10:53:41 -0700

    Do clang-format on startoptionssort.cpp Replace 20 lines of code with 1 line for loop

--- src/qt/startoptionssort.cpp
+++ src/qt/startoptionssort.cpp
@@ -2,104 +2,104 @@
 // Created by Kolby on 6/19/2019.
 //
 
-
-#include <startoptionssort.h>
-#include <ui_startoptionssort.h>
-#include <QtWidgets>
-#include <QTableWidget>
-#include <QListView>
-#include <QStringListModel>
 #include <QLineEdit>
+#include <QListView>
 #include <QListWidget>
+#include <QStringListModel>
+#include <QTableWidget>
+#include <QtWidgets>
 #include <algorithm>
-#include <random>
 #include <chrono>
 #include <dvtui.h>
+#include <random>
+#include <startoptionssort.h>
+#include <ui_startoptionssort.h>
 
-CustomRectItem::CustomRectItem(QGraphicsItem *parent):
-        QGraphicsRectItem(parent)
-{
+CustomRectItem::CustomRectItem(QGraphicsItem *parent)
+    : QGraphicsRectItem(parent) {
     setAcceptDrops(true);
     setFlags(QGraphicsItem::ItemIsSelectable);
 }
 
-void CustomRectItem::setText(const QString &text){
+void CustomRectItem::setText(const QString &text) {
     this->m_text = text;
     update();
 }
 
-void CustomRectItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-                           QWidget *widget){
+void CustomRectItem::paint(QPainter *painter,
+                           const QStyleOptionGraphicsItem *option,
+                           QWidget *widget) {
     QGraphicsRectItem::paint(painter, option, widget);
-    painter->drawText(boundingRect(),m_text,
-                      QTextOption(Qt::AlignCenter));
+    painter->drawText(boundingRect(), m_text, QTextOption(Qt::AlignCenter));
 }
 
-void CustomRectItem::dropEvent(QGraphicsSceneDragDropEvent *event){
-    if (event->mimeData()->hasFormat("application/x-qabstractitemmodeldatalist")){
-        QByteArray itemData = event->mimeData()->data(
-                "application/x-qabstractitemmodeldatalist");
+void CustomRectItem::dropEvent(QGraphicsSceneDragDropEvent *event) {
+    if (event->mimeData()->hasFormat(
+            "application/x-qabstractitemmodeldatalist")) {
+        QByteArray itemData =
+            event->mimeData()->data("application/x-qabstractitemmodeldatalist");
         QDataStream stream(&itemData, QIODevice::ReadOnly);
         int row, col;
         QMap<int, QVariant> valueMap;
         stream >> row >> col >> valueMap;
         QString origText = m_text;
-        if(!valueMap.isEmpty())
-            setText(valueMap.value(0).toString());
+        if (!valueMap.isEmpty()) setText(valueMap.value(0).toString());
 
         event->setDropAction(Qt::MoveAction);
         event->accept();
 
-        if(!origText.isEmpty()){
-            if(QListWidget* lWidget = qobject_cast<QListWidget*> (event->source()))
+        if (!origText.isEmpty()) {
+            if (QListWidget *lWidget =
+                    qobject_cast<QListWidget *>(event->source()))
                 lWidget->addItem(origText);
         }
-    }
-    else
+    } else
         event->ignore();
 }
 
-StartOptionsSort::StartOptionsSort(std::vector<std::string> Words, int rows, QWidget *parent)
-        : QWidget(parent), ui(new Ui::StartOptionsSort) {
+StartOptionsSort::StartOptionsSort(std::vector<std::string> Words, int rows,
+                                   QWidget *parent)
+    : QWidget(parent), ui(new Ui::StartOptionsSort) {
     ui->setupUi(this);
-    if(DVTUI::customThemeIsSet()) {
+    if (DVTUI::customThemeIsSet()) {
         QString appstyle = "fusion";
         QApplication::setStyle(appstyle);
         setStyleSheet(DVTUI::styleSheetString);
-    } 
+    }
 
     ui->dragdropLabel->setText(
-        tr("Please <b>drag</b> and <b>drop</b> your seed words into the correct order to confirm your recovery phrase. "));
+        tr("Please <b>drag</b> and <b>drop</b> your seed words into the "
+           "correct order to confirm your recovery phrase. "));
 
     scene = new QGraphicsScene(this);
-    if(rows == 4) {
-        scene->setSceneRect(0,0,620,229);
+    if (rows == 4) {
+        scene->setSceneRect(0, 0, 620, 229);
         view = new QGraphicsView;
         view->setScene(scene);
-        view->setMinimumSize(QSize(1,230));
+        view->setMinimumSize(QSize(1, 230));
     } else {
-        scene->setSceneRect(0,0,620,154);
+        scene->setSceneRect(0, 0, 620, 154);
         view = new QGraphicsView;
         view->setScene(scene);
-        view->setMinimumSize(QSize(1,155));
+        view->setMinimumSize(QSize(1, 155));
     }
-    
+
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-    QRectF rect(0,0,90,70);
-    QRectF rect2(0,0,90,2);
-    
-    for(int i=0; i<rows; i++){
-        for(int k=0; k<6; k++){
+    QRectF rect(0, 0, 90, 70);
+    QRectF rect2(0, 0, 90, 2);
+
+    for (int i = 0; i < rows; i++) {
+        for (int k = 0; k < 6; k++) {
             int ki;
-            if (k < 1){
+            if (k < 1) {
                 ki = 10;
             } else {
                 ki = 10 + 100 * k;
             }
             int ii;
-            if (i < 1){
+            if (i < 1) {
                 ii = 1;
             } else {
                 ii = 1 + 50 * i;
@@ -108,21 +108,20 @@ StartOptionsSort::StartOptionsSort(std::vector<std::string> Words, int rows, QWi
             CustomRectItem *listView = new CustomRectItem;
             scene->addItem(listView);
             listView->setRect(rect);
-            
+
             CustomRectItem *listViewBorder = new CustomRectItem;
             scene->addItem(listViewBorder);
             listViewBorder->setRect(rect2);
-            listViewBorder->setBrush(QColor(35,136,237));
-            listViewBorder->setPos(ki,ii + 50);
+            listViewBorder->setBrush(QColor(35, 136, 237));
+            listViewBorder->setPos(ki, ii + 50);
             listViewBorder->setPen(Qt::NoPen);
 
-            listView->setPos(ki,ii);
-            QPen myPen(QColor(255,255,255), 2, Qt::MPenStyle);
+            listView->setPos(ki, ii);
+            QPen myPen(QColor(255, 255, 255), 2, Qt::MPenStyle);
             listView->setPen(myPen);
-            
+
             graphicsList.push_back(listView);
             graphicsList.push_back(listViewBorder);
-            
         }
     }
     ui->gridLayoutRevealed->addWidget(view, 0, 0, 1, 6, Qt::AlignCenter);
@@ -130,62 +129,45 @@ StartOptionsSort::StartOptionsSort(std::vector<std::string> Words, int rows, QWi
     // Randomizes the word list
     std::vector<std::string> randomWords = Words;
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    shuffle (randomWords.begin(), randomWords.end(), std::default_random_engine(seed));
+    shuffle(randomWords.begin(), randomWords.end(),
+            std::default_random_engine(seed));
 
-        for(int k=0; k<6; k++){
+    for (int k = 0; k < 6; k++) {
 
-            QListWidget* itemListWidget = new QListWidget;
-            QStringList itemList;
-            if(rows == 4){
-                if(k<0){
-                    itemList.append(QString::fromStdString(randomWords[k]));
-                    itemList.append(QString::fromStdString(randomWords[k + 1]));
-                    itemList.append(QString::fromStdString(randomWords[k + 2]));
-                    itemList.append(QString::fromStdString(randomWords[k + 3]));
-                } else if(k<6) {
-                    itemList.append(QString::fromStdString(randomWords[k * 4]));
-                    itemList.append(QString::fromStdString(randomWords[k * 4 + 1]));
-                    itemList.append(QString::fromStdString(randomWords[k * 4 + 2]));
-                    itemList.append(QString::fromStdString(randomWords[k * 4 + 3]));
-                }
-            } else {
-                if(k<0){
-                    itemList.append(QString::fromStdString(randomWords[k]));
-                    itemList.append(QString::fromStdString(randomWords[k + 1]));
-                } else if(k<6) {
-                    itemList.append(QString::fromStdString(randomWords[k * 2]));
-                    itemList.append(QString::fromStdString(randomWords[k * 2 + 1]));
-                }
-            }
-
-            itemListWidget->addItems(itemList);
-            itemListWidget->setFixedWidth(120);
-            itemListWidget->setMinimumSize(QSize(118,180));
-            itemListWidget->setMaximumSize(QSize(118,180));
-            itemListWidget->setDragEnabled(true);
-            itemListWidget->setFocusPolicy(Qt::NoFocus);
-            itemListWidget->setStyleSheet("QWidget{font-size:18px; font-style:bold; color:#fff;}");
-            itemListWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-            labelsList.push_back(itemListWidget);
-            ui->gridLayoutRevealed->addWidget(itemListWidget, 2, k, Qt::AlignCenter);
+        QListWidget *itemListWidget = new QListWidget;
+        QStringList itemList;
+        for (int i = 0; i < rows; i++) {
+            itemList.append(QString::fromStdString(randomWords[k * rows + i]));
         }
 
+        itemListWidget->addItems(itemList);
+        itemListWidget->setFixedWidth(120);
+        itemListWidget->setMinimumSize(QSize(118, 180));
+        itemListWidget->setMaximumSize(QSize(118, 180));
+        itemListWidget->setDragEnabled(true);
+        itemListWidget->setFocusPolicy(Qt::NoFocus);
+        itemListWidget->setStyleSheet(
+            "QWidget{font-size:18px; font-style:bold; color:#fff;}");
+        itemListWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        labelsList.push_back(itemListWidget);
+        ui->gridLayoutRevealed->addWidget(itemListWidget, 2, k,
+                                          Qt::AlignCenter);
+    }
 
     setAcceptDrops(true);
     setWindowTitle(tr("Draggable Text"));
-
 }
 
-std::list<QString> StartOptionsSort::getOrderedStrings(){
+std::list<QString> StartOptionsSort::getOrderedStrings() {
     std::list<QString> list;
-    foreach(QGraphicsItem *item, scene->items())
-    if(CustomRectItem *rItem = qgraphicsitem_cast<CustomRectItem*> (item)) {
-        list.push_back(rItem->text());
-    }
+    foreach (QGraphicsItem *item, scene->items())
+        if (CustomRectItem *rItem =
+                qgraphicsitem_cast<CustomRectItem *>(item)) {
+            list.push_back(rItem->text());
+        }
     return list;
 }
 
 StartOptionsSort::~StartOptionsSort() {
     delete ui;
 }
-
