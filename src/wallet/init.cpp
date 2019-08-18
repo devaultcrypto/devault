d2b359c25648ed77313edac9b573f13861e27711
Jon Spock <jonspock@protonmail.com>
2019-08-18 14:02:33 -0700

    Add wallet bypass option

--- src/wallet/init.cpp
+++ src/wallet/init.cpp
@@ -61,6 +61,9 @@ void WalletInit::AddWalletOptions() const {
     gArgs.AddArg("-disablewallet",
                  _("Do not load the wallet and disable wallet RPC calls"),
                  false, OptionsCategory::WALLET);
+    gArgs.AddArg("-bypasspassword",
+               _("Set a default password of devaultpassword to bypass interactively setting it)"),
+                 false, OptionsCategory::WALLET);
     gArgs.AddArg("-keypool=<n>",
                  strprintf(_("Set key pool size to <n> (default: %u)"),
                            DEFAULT_KEYPOOL_SIZE),
