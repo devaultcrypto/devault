7a19839b7c5472e8f074c9601f16bf1e289c0235
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-09-13 17:53:06 -0300

    bench: Use non-throwing ParseDouble(...) instead of throwing boost::lexical_cast<double>(...)
    Summary:
    * Non-Boost is better than Boost.
    * Non-throwing is better than throwing.
    * Explicit error handling is better than implicit error handling.
    * `ParseDouble(…)` deserves to be used outside of its unit tests :-)
    
    Backport of Bitcoin Core PR13383
    https://github.com/bitcoin/bitcoin/pull/13383
    
    Test Plan:
    ```
    make check-all
    ```
    
    Reviewers: Fabien, #bitcoin_abc, deadalnix, nakihito
    
    Reviewed By: #bitcoin_abc, deadalnix, nakihito
    
    Differential Revision: https://reviews.bitcoinabc.org/D4080

--- src/bench/bench_bitcoin.cpp
+++ src/bench/bench_bitcoin.cpp
@@ -10,9 +10,8 @@
 #include <util.h>
 #include <fs_util.h>
 #include <validation.h>
-
-#include <boost/lexical_cast.hpp>
 #include <sodium/core.h>
+#include <utilstrencodings.h>
 
 #include <memory>
 
@@ -99,7 +98,12 @@ int main(int argc, char **argv) {
     std::string scaling_str = gArgs.GetArg("-scaling", DEFAULT_BENCH_SCALING);
     bool is_list_only = gArgs.GetBoolArg("-list", false);
 
-    double scaling_factor = boost::lexical_cast<double>(scaling_str);
+    double scaling_factor;
+    if (!ParseDouble(scaling_str, &scaling_factor)) {
+        fprintf(stderr, "Error parsing scaling factor as double: %s\n",
+                scaling_str.c_str());
+        return EXIT_FAILURE;
+    }
 
     std::unique_ptr<benchmark::Printer> printer(
         new benchmark::ConsolePrinter());
