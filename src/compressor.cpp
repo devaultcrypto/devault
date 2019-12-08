Bitcoin-ABC/bitcoin-abc

Merge #12752: [MOVEONLY] Move compressor utility functions out of class
Summary:
76a9aac Move compressor utility functions out of class (Pieter Wuille)

Pull request description:

  This is a refactor from #10785 with no functionality change.

  Move the compresion utility functions out of CScriptCompressor, as a preparation for making the class templated. I'm submitting it as a separate PR as I think it's a general improvement to code readability, and to reduce the diff further on.

Tree-SHA512: 3b3d17c2b96e43f752f512dd573296a6bb15cae165fbe3c79212a0970f5196a62a59a821d5100f29638af1e7461c9171f3dccb8112f005ee08da0ec7fe0073fd

Backport of Core PR12752
https://github.com/bitcoin/bitcoin/pull/12752/

Test Plan:
  make check

Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc

Reviewed By: Fabien, O1 Bitcoin ABC, #bitcoin_abc

Differential Revision: https://reviews.bitcoinabc.org/D3903

src/compressor.cpp
@@ -9,7 +9,15 @@
 #include <pubkey.h>
 #include <script/standard.h>
 
-bool CScriptCompressor::IsToKeyID(CKeyID &hash) const {
+/*
+ * These check for scripts for which a special case with a shorter encoding is
+ * defined. They are implemented separately from the CScript test, as these test
+ * for exact byte sequence correspondences, and are more strict. For example,
+ * IsToPubKey also verifies whether the public key is valid (as invalid ones
+ * cannot be represented in compressed form).
+ */
+
+static bool IsToKeyID(const CScript &script, CKeyID &hash) {
     if (script.size() == 25 && script[0] == OP_DUP && script[1] == OP_HASH160 &&
         script[2] == 20 && script[23] == OP_EQUALVERIFY &&
         script[24] == OP_CHECKSIG) {
@@ -19,7 +27,7 @@ bool CScriptCompressor::IsToKeyID(CKeyID &hash) const {
     return false;
 }
 
-bool CScriptCompressor::IsToScriptID(CScriptID &hash) const {
+static bool IsToScriptID(const CScript &script, CScriptID &hash) {
     if (script.size() == 23 && script[0] == OP_HASH160 && script[1] == 20 &&
         script[22] == OP_EQUAL) {
         memcpy(&hash, &script[2], 20);
@@ -28,7 +36,7 @@ bool CScriptCompressor::IsToScriptID(CScriptID &hash) const {
     return false;
 }
 
-bool CScriptCompressor::IsToPubKey(CPubKey &pubkey) const {
+static bool IsToPubKey(const CScript &script, CPubKey &pubkey) {
     if (script.size() == 35 && script[0] == 33 && script[34] == OP_CHECKSIG &&
         (script[1] == 0x02 || script[1] == 0x03)) {
         pubkey.Set(&script[1], &script[34]);
@@ -43,23 +51,23 @@ bool CScriptCompressor::IsToPubKey(CPubKey &pubkey) const {
     return false;
 }
 
-bool CScriptCompressor::Compress(std::vector<uint8_t> &out) const {
+bool CompressScript(const CScript &script, std::vector<uint8_t> &out) {
     CKeyID keyID;
-    if (IsToKeyID(keyID)) {
+    if (IsToKeyID(script, keyID)) {
         out.resize(21);
         out[0] = 0x00;
         memcpy(&out[1], &keyID, 20);
         return true;
     }
     CScriptID scriptID;
-    if (IsToScriptID(scriptID)) {
+    if (IsToScriptID(script, scriptID)) {
         out.resize(21);
         out[0] = 0x01;
         memcpy(&out[1], &scriptID, 20);
         return true;
     }
     CPubKey pubkey;
-    if (IsToPubKey(pubkey)) {
+    if (IsToPubKey(script, pubkey)) {
         out.resize(33);
         memcpy(&out[1], &pubkey[1], 32);
         if (pubkey[0] == 0x02 || pubkey[0] == 0x03) {
@@ -73,7 +81,7 @@ bool CScriptCompressor::Compress(std::vector<uint8_t> &out) const {
     return false;
 }
 
-unsigned int CScriptCompressor::GetSpecialSize(unsigned int nSize) const {
+unsigned int GetSpecialScriptSize(unsigned int nSize) {
     if (nSize == 0 || nSize == 1) {
         return 20;
     }
@@ -83,8 +91,8 @@ unsigned int CScriptCompressor::GetSpecialSize(unsigned int nSize) const {
     return 0;
 }
 
-bool CScriptCompressor::Decompress(unsigned int nSize,
-                                   const std::vector<uint8_t> &in) {
+bool DecompressScript(CScript &script, unsigned int nSize,
+                      const std::vector<uint8_t> &in) {
     switch (nSize) {
         case 0x00:
             script.resize(25);
@@ -138,7 +146,7 @@ bool CScriptCompressor::Decompress(unsigned int nSize,
 // * if e==9, we only know the resulting number is not zero, so output 1 + 10*(n
 // - 1) + 9
 // (this is decodable, as d is in [1-9] and e is in [0-9])
-uint64_t CTxOutCompressor::CompressAmount(Amount amt) {
+uint64_t CompressAmount(Amount amt) {
     uint64_t n = amt / SATOSHI;
     if (n == 0) {
         return 0;
@@ -158,7 +166,7 @@ uint64_t CTxOutCompressor::CompressAmount(Amount amt) {
     }
 }
 
-Amount CTxOutCompressor::DecompressAmount(uint64_t x) {
+Amount DecompressAmount(uint64_t x) {
     // x = 0  OR  x = 1+10*(9*n + d - 1) + e  OR  x = 1+10*(n - 1) + 9
     if (x == 0) {
         return Amount::zero();

src/compressor.h
@@ -14,6 +14,14 @@ class CKeyID;
 class CPubKey;
 class CScriptID;
 
+bool CompressScript(const CScript &script, std::vector<uint8_t> &out);
+unsigned int GetSpecialScriptSize(unsigned int nSize);
+bool DecompressScript(CScript &script, unsigned int nSize,
+                      const std::vector<uint8_t> &out);
+
+uint64_t CompressAmount(Amount nAmount);
+Amount DecompressAmount(uint64_t nAmount);
+
 /**
  * Compact serializer for scripts.
  *
@@ -38,28 +46,12 @@ private:
 
     CScript &script;
 
-protected:
-    /**
-     * These check for scripts for which a special case with a shorter encoding
-     * is defined. They are implemented separately from the CScript test, as
-     * these test for exact byte sequence correspondences, and are more strict.
-     * For example, IsToPubKey also verifies whether the public key is valid (as
-     * invalid ones cannot be represented in compressed form).
-     */
-    bool IsToKeyID(CKeyID &hash) const;
-    bool IsToScriptID(CScriptID &hash) const;
-    bool IsToPubKey(CPubKey &pubkey) const;
-
-    bool Compress(std::vector<uint8_t> &out) const;
-    unsigned int GetSpecialSize(unsigned int nSize) const;
-    bool Decompress(unsigned int nSize, const std::vector<uint8_t> &out);
-
 public:
     explicit CScriptCompressor(CScript &scriptIn) : script(scriptIn) {}
 
     template <typename Stream> void Serialize(Stream &s) const {
         std::vector<uint8_t> compr;
-        if (Compress(compr)) {
+        if (CompressScript(script, compr)) {
             s << CFlatData(compr);
             return;
         }
@@ -72,9 +64,9 @@ public:
         unsigned int nSize = 0;
         s >> VARINT(nSize);
         if (nSize < nSpecialScripts) {
-            std::vector<uint8_t> vch(GetSpecialSize(nSize), 0x00);
+            std::vector<uint8_t> vch(GetSpecialScriptSize(nSize), 0x00);
             s >> CFlatData(vch);
-            Decompress(nSize, vch);
+            DecompressScript(script, nSize, vch);
             return;
         }
         nSize -= nSpecialScripts;
@@ -95,9 +87,6 @@ private:
     CTxOut &txout;
 
 public:
-    static uint64_t CompressAmount(Amount nAmount);
-    static Amount DecompressAmount(uint64_t nAmount);
-
     explicit CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) {}
 
     ADD_SERIALIZE_METHODS;

devaultcrypto/devault

src/compressor.cpp
@@ -8,7 +8,15 @@
 #include <pubkey.h>
 #include <script/standard.h>
 
-bool CScriptCompressor::IsToKeyID(CKeyID &hash) const {
+/*
+ * These check for scripts for which a special case with a shorter encoding is
+ * defined. They are implemented separately from the CScript test, as these test
+ * for exact byte sequence correspondences, and are more strict. For example,
+ * IsToPubKey also verifies whether the public key is valid (as invalid ones
+ * cannot be represented in compressed form).
+ */
+
+static bool IsToKeyID(const CScript &script, CKeyID &hash) {
     if (script.size() == 25 && script[0] == OP_DUP && script[1] == OP_HASH160 &&
         script[2] == 20 && script[23] == OP_EQUALVERIFY &&
         script[24] == OP_CHECKSIG) {
@@ -18,7 +26,7 @@ bool CScriptCompressor::IsToKeyID(CKeyID &hash) const {
     return false;
 }
 
-bool CScriptCompressor::IsToScriptID(CScriptID &hash) const {
+static bool IsToScriptID(const CScript &script, CScriptID &hash) {
     if (script.size() == 23 && script[0] == OP_HASH160 && script[1] == 20 &&
         script[22] == OP_EQUAL) {
         memcpy(&hash, &script[2], 20);
@@ -27,7 +35,7 @@ bool CScriptCompressor::IsToScriptID(CScriptID &hash) const {
     return false;
 }
 
-bool CScriptCompressor::IsToPubKey(CPubKey &pubkey) const {
+static bool IsToPubKey(const CScript &script, CPubKey &pubkey) {
     if (script.size() == 35 && script[0] == 33 && script[34] == OP_CHECKSIG &&
         (script[1] == 0x02 || script[1] == 0x03)) {
         pubkey.Set(&script[1], &script[34]);
@@ -42,23 +50,23 @@ bool CScriptCompressor::IsToPubKey(CPubKey &pubkey) const {
     return false;
 }
 
-bool CScriptCompressor::Compress(std::vector<uint8_t> &out) const {
+bool CompressScript(const CScript &script, std::vector<uint8_t> &out) {
     CKeyID keyID;
-    if (IsToKeyID(keyID)) {
+    if (IsToKeyID(script, keyID)) {
         out.resize(21);
         out[0] = 0x00;
         memcpy(&out[1], &keyID, 20);
         return true;
     }
     CScriptID scriptID;
-    if (IsToScriptID(scriptID)) {
+    if (IsToScriptID(script, scriptID)) {
         out.resize(21);
         out[0] = 0x01;
         memcpy(&out[1], &scriptID, 20);
         return true;
     }
     CPubKey pubkey;
-    if (IsToPubKey(pubkey)) {
+    if (IsToPubKey(script, pubkey)) {
         out.resize(33);
         memcpy(&out[1], &pubkey[1], 32);
         if (pubkey[0] == 0x02 || pubkey[0] == 0x03) {
@@ -72,7 +80,7 @@ bool CScriptCompressor::Compress(std::vector<uint8_t> &out) const {
     return false;
 }
 
-unsigned int CScriptCompressor::GetSpecialSize(unsigned int nSize) const {
+unsigned int GetSpecialScriptSize(unsigned int nSize) {
     if (nSize == 0 || nSize == 1) {
         return 20;
     }
@@ -82,8 +90,8 @@ unsigned int CScriptCompressor::GetSpecialSize(unsigned int nSize) const {
     return 0;
 }
 
-bool CScriptCompressor::Decompress(unsigned int nSize,
-                                   const std::vector<uint8_t> &in) {
+bool DecompressScript(CScript &script, unsigned int nSize,
+                      const std::vector<uint8_t> &in) {
     switch (nSize) {
         case 0x00:
             script.resize(25);
@@ -137,7 +145,7 @@ bool CScriptCompressor::Decompress(unsigned int nSize,
 // * if e==9, we only know the resulting number is not zero, so output 1 + 10*(n
 // - 1) + 9
 // (this is decodable, as d is in [1-9] and e is in [0-9])
-uint64_t CTxOutCompressor::CompressAmount(uint64_t n) {
+uint64_t CompressAmount(uint64_t n) {
     if (n == 0) {
         return 0;
     }
@@ -156,7 +164,7 @@ uint64_t CTxOutCompressor::CompressAmount(uint64_t n) {
     }
 }
 
-int64_t CTxOutCompressor::DecompressAmount(uint64_t x) {
+int64_t DecompressAmount(uint64_t x) {
     // x = 0  OR  x = 1+10*(9*n + d - 1) + e  OR  x = 1+10*(n - 1) + 9
     if (x == 0) {
         return 0;

src/compressor.h
@@ -14,6 +14,14 @@ class CKeyID;
 class CPubKey;
 class CScriptID;
 
+bool CompressScript(const CScript &script, std::vector<uint8_t> &out);
+unsigned int GetSpecialScriptSize(unsigned int nSize);
+bool DecompressScript(CScript &script, unsigned int nSize,
+                      const std::vector<uint8_t> &out);
+
+uint64_t CompressAmount(uint64_t nAmount);
+int64_t DecompressAmount(uint64_t nAmount);
+
 /**
  * Compact serializer for scripts.
  *
@@ -38,28 +46,12 @@ private:
 
     CScript &script;
 
-protected:
-    /**
-     * These check for scripts for which a special case with a shorter encoding
-     * is defined. They are implemented separately from the CScript test, as
-     * these test for exact byte sequence correspondences, and are more strict.
-     * For example, IsToPubKey also verifies whether the public key is valid (as
-     * invalid ones cannot be represented in compressed form).
-     */
-    bool IsToKeyID(CKeyID &hash) const;
-    bool IsToScriptID(CScriptID &hash) const;
-    bool IsToPubKey(CPubKey &pubkey) const;
-
-    bool Compress(std::vector<uint8_t> &out) const;
-    unsigned int GetSpecialSize(unsigned int nSize) const;
-    bool Decompress(unsigned int nSize, const std::vector<uint8_t> &out);
-
 public:
     explicit CScriptCompressor(CScript &scriptIn) : script(scriptIn) {}
 
     template <typename Stream> void Serialize(Stream &s) const {
         std::vector<uint8_t> compr;
-        if (Compress(compr)) {
+        if (CompressScript(script, compr)) {
             s << CFlatData(compr);
             return;
         }
@@ -72,9 +64,9 @@ public:
         unsigned int nSize = 0;
         s >> VARINT(nSize);
         if (nSize < nSpecialScripts) {
-            std::vector<uint8_t> vch(GetSpecialSize(nSize), 0x00);
+            std::vector<uint8_t> vch(GetSpecialScriptSize(nSize), 0x00);
             s >> CFlatData(vch);
-            Decompress(nSize, vch);
+            DecompressScript(script, nSize, vch);
             return;
         }
         nSize -= nSpecialScripts;
@@ -95,9 +87,6 @@ private:
     CTxOut &txout;
 
 public:
-    static uint64_t CompressAmount(uint64_t nAmount);
-    static  int64_t DecompressAmount(uint64_t nAmount);
-
     explicit CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) {}
 
     ADD_SERIALIZE_METHODS;
@@ -110,7 +99,7 @@ public:
         } else {
             uint64_t nVal = 0;
             READWRITE(VARINT(nVal));
-            txout.nValue = Amount(int64_t(DecompressAmount(nVal)));
+            txout.nValue = Amount(DecompressAmount(nVal));
         }
         CScriptCompressor cscript(REF(txout.scriptPubKey));
         READWRITE(cscript);