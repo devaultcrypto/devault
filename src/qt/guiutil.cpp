8e0f7cce0ede345a9cf5be63163484bb3d00cd9c
Jon Spock <jonspock@protonmail.com>
2019-12-08 11:49:22 -0800

    Merge #12779: Qt: Remove unused method setupAmountWidget(...)

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -167,14 +167,6 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent) {
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
 }
 
-void setupAmountWidget(QLineEdit *widget, QWidget *parent) {
-    QDoubleValidator *amountValidator = new QDoubleValidator(parent);
-    amountValidator->setDecimals(8);
-    amountValidator->setBottom(0.0);
-    widget->setValidator(amountValidator);
-    widget->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-}
-
 QString bitcoinURIScheme(const CChainParams &params) {
     return QString::fromStdString(params.CashAddrPrefix());
 }
