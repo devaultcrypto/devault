Bitcoin-ABC/bitcoin-abc

Remove GetNextBlockScriptFlags's requirement to hold cs_main
Summary: It turns out this is not needed.

Test Plan: Build with clang, check there's no warning.

Reviewers: #bitcoin_abc, Fabien

Reviewed By: #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D4100

src/validation.cpp
@@ -330,8 +330,7 @@ static FILE *OpenUndoFile(const FlatFilePos &pos, bool fReadOnly = false);
 static FlatFileSeq BlockFileSeq();
 static FlatFileSeq UndoFileSeq();
 static uint32_t GetNextBlockScriptFlags(const Consensus::Params &params,
-                                        const CBlockIndex *pindex)
-    EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+                                        const CBlockIndex *pindex);
 
 bool TestLockPointValidity(const LockPoints *lp) {
     AssertLockHeld(cs_main);
@@ -1598,9 +1597,7 @@ int32_t ComputeBlockVersion(const CBlockIndex *pindexPrev,
 // Returns the script flags which should be checked for the block after
 // the given block.
 static uint32_t GetNextBlockScriptFlags(const Consensus::Params &params,
-                                        const CBlockIndex *pindex)
-    EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
-    AssertLockHeld(cs_main);
+                                        const CBlockIndex *pindex) {
     uint32_t flags = SCRIPT_VERIFY_NONE;
 
     // Start enforcing P2SH (BIP16)

devaultcrypto/devault

src/validation.cpp
@@ -240,7 +240,7 @@ static void FindFilesToPrune(std::set<int> &setFilesToPrune,
                              uint64_t nPruneAfterHeight);
 static FILE *OpenUndoFile(const CDiskBlockPos &pos, bool fReadOnly = false);
 static uint32_t GetBlockScriptFlags(const Config &config,
-                                    const CBlockIndex *pChainTip) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+                                    const CBlockIndex *pChainTip);
 
 bool TestLockPointValidity(const LockPoints *lp) {
     AssertLockHeld(cs_main);
@@ -1580,10 +1580,9 @@ int32_t ComputeBlockVersion(const CBlockIndex *pindexPrev,
 
 // Returns the script flags which should be checked for a given block
 static uint32_t GetBlockScriptFlags(const Config &config,
-                                    const CBlockIndex *pChainTip) EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
+                                    const CBlockIndex *pChainTip) {
     AssertLockHeld(cs_main);
     //const Consensus::Params &consensusParams = config.GetChainParams().GetConsensus();
-
     uint32_t flags = SCRIPT_VERIFY_NONE;
 
     flags |= SCRIPT_VERIFY_P2SH;