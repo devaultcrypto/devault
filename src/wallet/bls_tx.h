277ff24ae38c7b18ba9ecad39ace51a327a233cf
Jon Spock <jonspock@protonmail.com>
2020-02-16 23:19:04 -0800

    Adding new files for BLS update

---
+++ src/wallet/bls_tx.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 The DeVault developers
+// Copyright (c) 2020 Jon Spock
+// Distributed under the MIT software license, see the accompanying
+
+// Although these functions require interacting with wallet, they have been kept
+// outside of the class due to minimal interaction. Wallet is required for the
+// private keys used.
+
+#pragma once
+
+#include <optional>
+#include <set>
+#include <string>
+#include <utility>
+
+class CWallet;
+class CInputCoin;
+class CMutableTransaction;
+
+auto CreatePrivateTxWithSig(const CWallet *pwallet, const std::set<CInputCoin> &setCoins, CMutableTransaction &txNew)
+    -> std::optional<std::string>;
