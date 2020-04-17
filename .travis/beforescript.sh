86650469ec51cb2230c5282554f199922c6c6d7d
Akshay CM <akshaycm@hotmail.com>
2020-04-17 05:16:25 +0300

    Add inital docker based gitian builder to travis (#269)
    Dont build depends on build_gitian
    
    Remove undefined check_mac
    
    clone the dvt repo in gitian build
    
    Fix descriptors path

--- .travis/beforescript.sh
+++ .travis/beforescript.sh
@@ -5,17 +5,21 @@ export LC_ALL=C.UTF-8
 DOCKER_EXEC echo \> \$HOME/.devault
 
 mkdir -p depends/SDKs depends/sdk-sources
-
-if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
-  curl --location --fail $SDK_URL/MacOSX${OSX_SDK}.sdk.tar.gz -o depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
+if [ -z "$BUILD_GITIAN" ]
+then
+  echo 'building depends as we are testing native builds'
+  if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
+    curl --location --fail $SDK_URL/MacOSX${OSX_SDK}.sdk.tar.gz -o depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
+  fi
+  if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
+    tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
+  fi
+  if [[ $HOST = *-mingw32 ]]; then
+    DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
+  fi
+  if [ -z "$NO_DEPENDS" ]; then
+    DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
+  fi
+else
+  echo 'Not building depends as we are testing gitian builds'
 fi
-if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
-  tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
-fi
-if [[ $HOST = *-mingw32 ]]; then
-  DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
-fi
-if [ -z "$NO_DEPENDS" ]; then
-  DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
-fi
-
