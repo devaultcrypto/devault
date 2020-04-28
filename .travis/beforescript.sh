9649d253f59b9a0328e980b93960e2d397d27000
Spock <jonspock@protonmail.com>
2020-04-27 19:32:12 -0700

    Change travis for cross compiles (#271)
    use debian for Win64

--- .travis/beforescript.sh
+++ .travis/beforescript.sh
@@ -5,21 +5,18 @@ export LC_ALL=C.UTF-8
 DOCKER_EXEC echo \> \$HOME/.devault
 
 mkdir -p depends/SDKs depends/sdk-sources
-if [ -z "$BUILD_GITIAN" ]
-then
-  echo 'building depends as we are testing native builds'
-  if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
-    curl --location --fail $SDK_URL/MacOSX${OSX_SDK}.sdk.tar.gz -o depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
-  fi
-  if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
-    tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
-  fi
-  if [[ $HOST = *-mingw32 ]]; then
-    DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
-  fi
-  if [ -z "$NO_DEPENDS" ]; then
-    DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
-  fi
-else
-  echo 'Not building depends as we are testing gitian builds'
+
+if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
+  curl --location --fail $SDK_URL/MacOSX${OSX_SDK}.sdk.tar.gz -o depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
 fi
+if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
+  tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
+fi
+if [[ $HOST = *-mingw32 ]]; then
+  DOCKER_EXEC update-alternatives --set $HOST-g++ /usr/bin/$HOST-g++-posix
+  DOCKER_EXEC update-alternatives --set $HOST-gcc /usr/bin/$HOST-gcc-posix
+fi
+if [ -z "$NO_DEPENDS" ]; then
+  DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
+fi
+
