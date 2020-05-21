a4bec9a68d6dd44954fe00bad58ff4bd861482fd
Jon Spock <jonspock@protonmail.com>
2020-05-21 08:50:00 -0700

    Update BLS/Relic files
    Revert "Disable a BLS test - review later"
    
    This reverts commit 6c15fbab76995cb2b5006492c2b5825e33d2d6fb.

--- src/bls/relic/src/rand/relic_rand_core.c
+++ src/bls/relic/src/rand/relic_rand_core.c
@@ -1,23 +1,24 @@
 /*
  * RELIC is an Efficient LIbrary for Cryptography
- * Copyright (C) 2007-2017 RELIC Authors
+ * Copyright (C) 2007-2019 RELIC Authors
  *
  * This file is part of RELIC. RELIC is legal property of its developers,
  * whose names are not listed here. Please refer to the COPYRIGHT file
  * for contact information.
  *
- * RELIC is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * RELIC is free software; you can redistribute it and/or modify it under the
+ * terms of the version 2.1 (or later) of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; or version 2.0 of the Apache
+ * License as published by the Apache Software Foundation. See the LICENSE files
+ * for more details.
  *
- * RELIC is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * RELIC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the LICENSE files for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with RELIC. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public or the
+ * Apache License along with RELIC. If not, see <https://www.gnu.org/licenses/>
+ * or <https://www.apache.org/licenses/>.
  */
 
 /**
@@ -39,23 +40,23 @@
 #include "relic_md.h"
 #include "relic_err.h"
 
-#if RELICRAND == UDEV || RELICSEED == DEV || RELICSEED == UDEV
+#if RAND == UDEV || SEED == DEV || SEED == UDEV
 
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#elif RELICSEED == WCGR
+#elif SEED == WCGR
 
 /* Avoid redefinition warning. */
 #undef ERROR
-#undef WORD
+#undef WSIZE
 #undef DOUBLE
 
 #include <windows.h>
 #include <wincrypt.h>
 
-#elif RELICSEED == RDRND
+#elif SEED == RDRND
 
 #include <immintrin.h>
 
@@ -68,10 +69,10 @@
 /**
  * The path to the char device that supplies entropy.
  */
-#if RELICSEED == DEV
-#define RELICRAND_PATH		"/dev/random"
+#if SEED == DEV
+#define RAND_PATH		"/dev/random"
 #else
-#define RELICRAND_PATH		"/dev/urandom"
+#define RAND_PATH		"/dev/urandom"
 #endif
 
 /*============================================================================*/
@@ -84,20 +85,20 @@ void rand_init(void) {
 #if RELICRAND == UDEV
 	int *fd = (int *)&(core_get()->rand);
 
-	*fd = open(RELICRAND_PATH, O_RDONLY);
+	*fd = open(RAND_PATH, O_RDONLY);
 	if (*fd == -1) {
 		THROW(ERR_NO_FILE);
 	}
 #else
 
-#if RELICSEED == ZERO
+#if !defined(SEED)
 
 	memset(buf, 0, SEED_SIZE);
 
-#elif RELICSEED == DEV || RELICSEED == UDEV
+#elif SEED == DEV || SEED == UDEV
 	int fd, c, l;
 
-	fd = open(RELICRAND_PATH, O_RDONLY);
+	fd = open(RAND_PATH, O_RDONLY);
 	if (fd == -1) {
 		THROW(ERR_NO_FILE);
 	}
@@ -114,21 +115,23 @@ void rand_init(void) {
 	if (fd != -1) {
 		close(fd);
 	}
-#elif RELICSEED == LIBC
+#elif SEED == LIBC
 
 #if OPSYS == FREEBSD
-	srandom(1);
+	/* This is better than using a fixed value. */
+	srandomdev();
 	for (int i = 0; i < SEED_SIZE; i++) {
 		buf[i] = (uint8_t)random();
 	}
 #else
+	/* This is horribly insecure, serves only for benchmarking. */
 	srand(1);
 	for (int i = 0; i < SEED_SIZE; i++) {
 		buf[i] = (uint8_t)rand();
 	}
 #endif
 
-#elif RELICSEED == WCGR
+#elif SEED == WCGR
 
 	HCRYPTPROV hCryptProv;
 
@@ -143,7 +146,7 @@ void rand_init(void) {
 		THROW(ERR_NO_READ);
 	}
 
-#elif RELICSEED == RDRND
+#elif SEED == RDRND
 
 	int i, j;
 	ull_t r;
@@ -161,7 +164,7 @@ void rand_init(void) {
 
 #endif
 
-#endif /* RELICRAND == UDEV */
+#endif /* RAND == UDEV */
 
 #if RELICRAND != CALL
 	core_get()->seeded = 0;
