e851d6b3bd1fc5a8bf81b551b2bb1d32a2772770
Jon Spock <jonspock@protonmail.com>
2020-05-30 13:42:49 -0700

    Try AugScheme - refactor bls_functions - untested

--- src/bls/bls_functions.cpp
+++ src/bls/bls_functions.cpp
@@ -4,6 +4,7 @@
 #include "bls/bls.hpp"
 #include "bls/privatekey.hpp"
 #include "bls/signature.hpp"
+#include "bls/schemes.hpp"
 #include "util/strencodings.h"
 #include <bls/bls_functions.h>
 #include <cstring>
@@ -14,111 +15,34 @@ namespace bls {
 
 bool SignBLS(const CKey &key, const uint256 &hash, std::vector<uint8_t> &vchSig) {
   auto PK = bls::PrivateKey::FromSeed(key.begin(), PrivateKey::PRIVATE_KEY_SIZE);
-  bls::Signature sig = PK.SignPrehashed(hash.begin());
-  uint8_t sigBytes[bls::Signature::SIGNATURE_SIZE]; // 96 byte array
-  sig.Serialize(sigBytes);
-  vchSig.resize(bls::Signature::SIGNATURE_SIZE);
-  for (size_t i = 0; i < bls::Signature::SIGNATURE_SIZE; i++)
-    vchSig[i] = sigBytes[i];
+  std::vector<uint8_t> message(hash.begin(),hash.end());
+  vchSig = AugScheme::Sign(PK, message);
   // Then Verify
-  return sig.Verify();
+  return true; // for now True - sig.Verify();
 }
 
 auto SignBLS(const CKey &key, const uint256 &hash) -> std::optional<std::vector<uint8_t>> {
   auto PK = bls::PrivateKey::FromSeed(key.begin(), PrivateKey::PRIVATE_KEY_SIZE);
-  bls::Signature sig = PK.SignPrehashed(hash.begin());
-  uint8_t sigBytes[bls::Signature::SIGNATURE_SIZE]; // 96 byte array
-  sig.Serialize(sigBytes);
-  bool ok = sig.Verify();
-  if (!ok) return std::nullopt;
-  std::vector<uint8_t> vchSig(bls::Signature::SIGNATURE_SIZE);
-  for (size_t i = 0; i < bls::Signature::SIGNATURE_SIZE; i++)
-    vchSig[i] = sigBytes[i];
+  std::vector<uint8_t> message(hash.begin(),hash.end());
+  auto vchSig = AugScheme::Sign(PK, message);
   return vchSig;
 }
-
+  
 bool SignBLS(const CKey &key, const uint256 &hash, Signature &sig) {
   auto PK = bls::PrivateKey::FromSeed(key.begin(), PrivateKey::PRIVATE_KEY_SIZE);
-  sig = PK.SignPrehashed(hash.begin());
-  // Then Verify
-  return sig.Verify();
+  std::vector<uint8_t> message(hash.begin(),hash.end());
+  auto vchSig = AugScheme::Sign(PK, message);
+  sig = bls::Signature::FromBytes(vchSig.data());
+  return true; // for now
 }
-/*
-bool SignBLSMessage(const CKey &key, const std::vector<uint8_t> &message, Signature& sig) {
-  auto PK = bls::PrivateKey::FromSeed(key.begin(), PrivateKey::PRIVATE_KEY_SIZE);
-  sig = PK.Sign(message.data(),message.size());
-  // Then Verify
-  return sig.Verify();
-}
-*/
-bool VerifyBLS(const uint256 &hash, const std::vector<uint8_t> &vchSig, const uint8_t *vch) {
-  try {
-    bls::Signature sig = bls::Signature::FromBytes(vchSig.data());
-    // Add information required for verification, to sig object
-    
-    // Unserialize from CPubKey back to PublicKey
-    bls::PublicKey Pk = bls::PublicKey::FromBytes(vch);
-    
-    sig.SetAggregationInfo(bls::AggregationInfo::FromMsgHash(Pk, hash.begin()));
-
-    bool ok = sig.Verify();
-    return ok;
-  } catch (...) { throw std::runtime_error("Problem aggregating signatures"); }
   
-}
+bool VerifyBLS(const uint256 &hash, const std::vector<uint8_t> &vchSig, const uint8_t *vch) {
 
-bool VerifyBLS(const uint256 &hash, Signature &sig, const uint8_t *vch) {
-
-  // Unserialize from CPubKey back to PublicKey
-  bls::PublicKey Pk = bls::PublicKey::FromBytes(vch);
-  sig.SetAggregationInfo(bls::AggregationInfo::FromMsgHash(Pk, hash.begin()));
-  bool ok = sig.Verify();
-  return ok;
-}
-
-// aggregate and convert to std::vector<uint8_t>
-std::vector<uint8_t> AggregateSig(std::vector<CKey> &keys, const uint256 &hash) {
-  bool sigsok = true;
-  std::vector<Signature> sigs;
-  std::vector<uint8_t> aggSigsAsBytes;
-  for (size_t i = 0; i < keys.size(); i++) {
-    bls::Signature Sig;
-    if (SignBLS(keys[i], hash, Sig))
-      sigs.push_back(Sig);
-    else
-      sigsok = false;
-  }
-  if (!sigsok) return std::vector<uint8_t>();
-  try {
-    Signature aggSig = Signature::Aggregate(sigs);
-    aggSigsAsBytes = aggSig.Serialize();
-  } catch (...) { throw std::runtime_error("Problem aggregating signatures"); }
-  return aggSigsAsBytes;
-}
-
-bool VerifyAggregate(const uint256 &hash, const std::vector<uint8_t> &aggSig, const std::vector<uint8_t> &aggPubKey) {
-  try {
-    // Unserialize from CPubKey back to PublicKey
-    bls::PublicKey Pk = bls::PublicKey::FromBytes(aggPubKey.data());
-    bls::Signature Sig = bls::Signature::FromBytes(aggSig.data());
-    Sig.SetAggregationInfo(bls::AggregationInfo::FromMsgHash(Pk, hash.begin()));
-    bool ok = Sig.Verify();
-    return ok;
-  }
-  catch (...) { return false; } // Don't crash!
-}
-
-// Convert from std::vector<uint8_t> , aggregate and convert back
-std::vector<uint8_t> AggregatePubKeys(std::vector<std::vector<uint8_t>> &vPubKeys) {
-  if (vPubKeys.size() == 1) return vPubKeys[0];
-  std::vector<bls::PublicKey> pubkeys;
-  for (size_t i = 0; i < vPubKeys.size(); i++) {
-    bls::PublicKey p = bls::PublicKey::FromBytes(vPubKeys[i].data()); // check
-    pubkeys.push_back(p);
-  }
-
-  bls::PublicKey aggPubKey = bls::PublicKey::Aggregate(pubkeys);
-  return aggPubKey.Serialize();
+  auto pub = bls::PublicKey::FromBytes(vch); //???
+  auto pubkey = pub.Serialize();
+  std::vector<uint8_t> message(hash.begin(),hash.end());
+  return AugScheme::Verify(pubkey,message,vchSig);
+  
 }
 
 CPubKey GetBLSPublicKey(const CKey &key) {
@@ -138,112 +62,54 @@ CPubKey GetBLSPublicKey(const CKey &key) {
 }
 
 //--------------------------------------------------------------------------------------------------
-
-// aggregate and convert to std::vector<uint8_t>
-std::vector<uint8_t> AggregateSigForMessages(std::vector<CKey> &keys, const std::vector<uint256> &hash) {
-  bool sigsok = true;
-  assert(keys.size() == hash.size());
-  std::vector<Signature> sigs;
-  std::vector<uint8_t> aggSigsAsBytes;
-  for (size_t i = 0; i < keys.size(); i++) {
-    bls::Signature Sig;
-    if (SignBLS(keys[i], hash[i], Sig))
-      sigs.push_back(Sig);
-    else
-      sigsok = false;
+// Convert from std::vector<uint8_t> , aggregate and convert back
+std::vector<uint8_t> AggregatePubKeys(std::vector<std::vector<uint8_t>> &vPubKeys) {
+  if (vPubKeys.size() == 1) return vPubKeys[0];
+  std::vector<bls::PublicKey> pubkeys;
+  for (size_t i = 0; i < vPubKeys.size(); i++) {
+    bls::PublicKey p = bls::PublicKey::FromBytes(vPubKeys[i].data()); // check
+    pubkeys.push_back(p);
   }
-  if (!sigsok) return std::vector<uint8_t>();
-  try {
-    Signature aggSig = Signature::Aggregate(sigs);
-    aggSigsAsBytes = aggSig.Serialize();
-  } catch (...) { throw std::runtime_error("Problem aggregating signatures"); }
-  return aggSigsAsBytes;
+
+  bls::PublicKey aggPubKey = bls::PublicKey::Aggregate(pubkeys);
+  return aggPubKey.Serialize();
 }
 
 // aggregate and convert to std::vector<uint8_t>
 std::vector<uint8_t> AggregateSigForMessages(std::map<uint256, CKey> &keys_plus_hash) {
   bool sigsok = true;
-  std::vector<Signature> sigs;
-  std::vector<uint8_t> aggSigsAsBytes;
+  std::vector<std::vector<uint8_t>> sigs;
   for (const auto &kph : keys_plus_hash) {
-    bls::Signature Sig;
+    std::vector<uint8_t> Sig;
     if (SignBLS(kph.second, kph.first, Sig))
       sigs.push_back(Sig);
     else
       sigsok = false;
-    /*
-    {
-      std::vector<uint8_t> vchSig(bls::Signature::SIGNATURE_SIZE);
-      uint8_t sigBytes[bls::Signature::SIGNATURE_SIZE]; // 96 byte array
-      Sig.Serialize(sigBytes);
-      for (size_t i = 0; i < bls::Signature::SIGNATURE_SIZE; i++) vchSig[i] = sigBytes[i];
-      std::cout << "hash = " << kph.first.ToString()
-                << " addr = " << EncodeDestination(kph.second.GetPubKeyForBLS().GetBLSKeyID()) << " "
-                << " pub = " << HexStr(ToByteVector(kph.second.GetPubKeyForBLS())) << " "
-                << " secret addr = " << EncodeSecret(kph.second) << " "
-                << " secret = " << HexStr(kph.second.begin(),kph.second.end()) << " "
-                << "sig = " << HexStr(vchSig) << "\n";
-    }
-    */
   }
   if (!sigsok) return std::vector<uint8_t>();
-  try {
-    Signature aggSig = Signature::Aggregate(sigs);
-    aggSigsAsBytes = aggSig.Serialize();
-    //std::cout << "AggSig = " << HexStr(aggSigsAsBytes) << "\n";
 
-    
-  } catch (...) { throw std::runtime_error("Problem aggregating signatures"); }
-  return aggSigsAsBytes;
+  std::vector<uint8_t> aggSig = AugScheme::Aggregate(sigs);
+  return aggSig;
+}
+
+  
+bool VerifySigForMessages(const std::vector<uint256> &msgs, const std::vector<uint8_t> &aggSigs,
+                          const std::vector<std::vector<uint8_t>> &pubkeys) {
+
+  std::vector<std::vector<uint8_t>> messages;
+  for (const auto& m : msgs) {
+    std::vector<uint8_t> ms(m.begin(),m.end());
+    messages.push_back(ms);
+  }
+  return AugScheme::AggregateVerify(pubkeys, messages, aggSigs);
 }
 
 std::vector<uint8_t> MakeAggregateSigsForMessages(const std::vector<uint256> &msgs,
                                                   const std::vector<std::vector<uint8_t>> &aggSigs,
                                                   const std::vector<std::vector<uint8_t>> &pubkeys) {
 
-  std::vector<bls::Signature> sigs;
-  std::vector<uint8_t> aggSigsAsBytes;
-  int N = aggSigs.size();
-  int MsgsPerN = msgs.size()/N;
-    
-  int inc = 0;
-  for (int i=0;i<N;i++) {
-      std::vector<bls::AggregationInfo> infos;
-      for (int j=0;j<MsgsPerN;j++) {
-          bls::PublicKey pk = bls::PublicKey::FromBytes(pubkeys[inc].data());
-          auto info = bls::AggregationInfo::FromMsgHash(pk, msgs[inc++].begin());
-          infos.push_back(info);
-      }
-      bls::Signature sig = bls::Signature::FromBytes(aggSigs[i].data());
-      bls::AggregationInfo mergedInfo = bls::AggregationInfo::MergeInfos(infos);
-      sig.SetAggregationInfo(mergedInfo);
-      sigs.push_back(sig);
-  }
-  try {
-      bls::Signature aggSig = bls::Signature::Aggregate(sigs);
-      aggSigsAsBytes = aggSig.Serialize();
-  } catch (...) { throw std::runtime_error("Problem aggregating signatures"); }
-  return aggSigsAsBytes;
+  auto Agg = AugScheme::Aggregate(aggSigs);
+  return Agg;
 }
     
-bool VerifySigForMessages(const std::vector<uint256> &msgs, const std::vector<uint8_t> &aggSigs,
-                          const std::vector<std::vector<uint8_t>> &pubkeys) {
-
-  try {
-    auto aggSigFinal = bls::Signature::FromBytes(aggSigs.data());
-    std::vector<bls::AggregationInfo> infos;
-    int i = 0;
-    for (const auto &msg : msgs) {
-      bls::PublicKey Pk = bls::PublicKey::FromBytes(pubkeys[i++].data());
-      infos.push_back(bls::AggregationInfo::FromMsgHash(Pk, msg.begin()));
-    }
-
-    auto aFinal = bls::AggregationInfo::MergeInfos(infos);
-    aggSigFinal.SetAggregationInfo(aFinal);
-    bool ok = aggSigFinal.Verify();
-    return ok;
-  }
-  catch (...) { return false; } // Don't crash!
-}
-
 } // namespace bls
