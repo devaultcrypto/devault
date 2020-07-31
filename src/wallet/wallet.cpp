1309bb35770906a3390f4607c8a36b5582dbf94f
Jon Spock <jonspock@protonmail.com>
2020-07-31 06:49:35 -0700

    Change calculation for CalculateMaximumSignedTxSize so that it doesn't return a size too small

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1538,7 +1538,17 @@ int64_t CalculateMaximumSignedTxSize(const CTransaction &tx,
         // implies that we can sign for every input.
         return -1;
     }
-    return GetVirtualTransactionSize(CTransaction(txNew));
+    // This is formula for BLS Txes
+    // will overestimate size for EC Tx when more outputs
+    int64_t bls_size = 91 * tx.vin.size() + 83 * tx.vout.size() + 110;
+    // This is formula for EC Txes
+    // will overestimate size for BLS Tx when more inputs
+    int64_t ec_size = 146 * tx.vin.size() + 33 * tx.vout.size() + 10;
+    // For now return the larger of the two, this will mean potentially
+    // more fees than needed but sizes should be comparable and this
+    // will prevent issues with fees being too low
+    int64_t size = std::max(bls_size,ec_size);
+    return size;
 }
 
 int CalculateMaximumSignedInputSize(const CTxOut &txout,
