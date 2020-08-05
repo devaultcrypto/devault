eb7ef16e853e5c04823dcac5dc865905998cc58f
Jon Spock <jonspock@protonmail.com>
2020-08-05 15:29:29 -0700

    restore Ancestor/Descendant consts and override in CalculateMemPoolAncestors for BLS Txes

--- src/validation.h
+++ src/validation.h
@@ -72,15 +72,15 @@ static const Amount HIGH_TX_FEE_PER_KB(100*COIN);
  */
 static const Amount HIGH_MAX_TX_FEE(100 * HIGH_TX_FEE_PER_KB);
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
-static const unsigned int DEFAULT_ANCESTOR_LIMIT = 0;
+static const unsigned int DEFAULT_ANCESTOR_LIMIT = 25;
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool
  * ancestors */
-static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 0;
+static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 101;
 /** Default for -limitdescendantcount, max number of in-mempool descendants */
-static const unsigned int DEFAULT_DESCENDANT_LIMIT = 0;
+static const unsigned int DEFAULT_DESCENDANT_LIMIT = 25;
 /** Default for -limitdescendantsize, maximum kilobytes of in-mempool
  * descendants */
-static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 0;
+static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 101;
 /** Default for -mempoolexpiry, expiration time for mempool transactions in
  * hours */
 static const unsigned int DEFAULT_MEMPOOL_EXPIRY = 336;
