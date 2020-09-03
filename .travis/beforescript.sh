476a0da48e7d7f206c0be495bc4812b0cb6e4024
Proteus <pro@proteanx.com>
2020-09-03 15:19:26 -0400

    additional deps + repos

--- .travis/beforescript.sh
+++ .travis/beforescript.sh
@@ -7,6 +7,9 @@ DOCKER_EXEC echo \> \$HOME/.devault
 mkdir -p depends/SDKs depends/sdk-sources
 
 if [[ $HOST = x86_64-apple-darwin14 ]]; then
+  DOCKER_EXEC apt install software-properties-common -y
+  DOCKER_EXEC add-apt-repository ppa:hnakamur/libarchive -y
+  DOCKER_EXEC add-apt-repository ppa:hnakamur/libzstd -y
   DOCKER_EXEC add-apt-repository ppa:hnakamur/cmake -y
 fi
 
